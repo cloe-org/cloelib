1d61c54c987bcb625cf0668d2e8b587d93b54064
marcobonici <bonici.marco@gmail.com>
2024-06-12 11:54:52 -0400

    Cleanup

--- cloelite/observables/two_point.py
+++ cloelite/observables/two_point.py
@@ -38,19 +38,12 @@ class AngularTwoPoint(TwoPoint):
         chi = self.tracer1.background.comoving_distance(zs_calc)
 
         chi2 = chi**2
+
         Pk = jax.vmap(self.tracer1.perturbations.nonlinear_matter_power_spectrum,
                       in_axes = (0, None))(ks, zs_calc)
-        #pmm_logspline = RectBivariateSpline(np.log10(ks), zs_calc, np.log10(Pk),
-        #                                    kx = 3, ky = 3, s = 0)
-        #Pkl = np.zeros((nl, len(zs_calc)))
         k_lz = np.expand_dims((ells + 0.5), 1) / chi
         Pkl = Pkl_interp_vmap(k_lz, zs_calc, ks, zs_calc, Pk)
-        #for (z_idx, myz) in enumerate(zs_calc):
-        #    Pkl[:, z_idx] = 10**pmm_logspline(np.log10(k_lz[:, z_idx]), myz)[:,0]
-        #for (z_idx, myz) in enumerate(zs_calc):
-        #    Pkl[:, z_idx] = 10**interpax.interp2d(np.log10(k_lz[:, z_idx]), myz,
-        #                                          np.log10(ks), zs_calc, np.log10(Pk),
-        #                                          method="cubic")
+
         WT1 = self.tracer1.get_window_positions(zs_calc)
         WT2 = self.tracer1.get_window_positions(zs_calc)
         result = np.einsum('iz,jz,lz,z,z->lij', WT1, WT2, Pkl, 1/H, 1/chi2)

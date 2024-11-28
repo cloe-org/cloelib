820e6cd4e7d9c89e35aebc4105d0792babdbad64
Guadalupe Canas-Herrera <guadalupe.canasherrera@esa.int>
2024-11-28 23:08:30 +0100

    changed abstract classes for protocols

--- cloelite/cosmology/cosmology.py
+++ cloelite/cosmology/cosmology.py
@@ -1,295 +1,107 @@
 # General imports
-import numpy as np
-from abc import ABC, abstractmethod
+from typing import Protocol
+import numpy as np  # type: ignore
 
 """
-
 ## Notes:
+ 
+- Refactored cosmology.py from the original CLOE to provide a more flexible framework,
+  enabling seamless integration with external cosmological codes while removing dependency on Cobaya.
 
-- Fix cosmology.py from original CLOE by offering a more flexible
-approach to link to other codes and make it Cobaya independent
-
+- Introduced the use of protocols to standardize external code interfaces,
+  providing a unified and extensible template for interaction.
 """
 
-class Background(ABC):
-    def __init__(self, H0: float, Omb: float, Omc: float, Omk: float, As: float, ns: float,
-                 w: float, wa: float, sigma8 : float, gamma_MG: float):
-        self.H0 = float(H0)
-        self.Omb = float(Omb)
-        self.Omc = float(Omc)
-        self.Omk = float(Omk)
-        self.As = float(As)
-        self.sigma8 = float(sigma8)
-        self.ns = float(ns)
-        self.w = float(w)
-        self.wa = float(wa)
-        self.gamma_MG = float(gamma_MG)
+class Background(Protocol):
+    H0: float
+    Omb: float
+    Omc: float
+    Omk: float
+    As: float
+    ns: float
+    w: float
+    wa: float
+    gamma_MG: float
+    # Variable to be able to keep args from background codes to perturbations
+    # in reality, will we use it beyond CAMB/CLASS?
+    # If it is not an array, not auto-diff
+    interface_args: None
 
-    @abstractmethod
-    def hubble_parameter(self, zs, units = '1/Mpc'):
-        r"""
-        Retrieves the hubble parameter as
-        a function of redshift
-
-        .. math::
-            H(z) = \sqrt
-
-        Parameters
-        ----------
-        zs: numpy.ndarray
-            Redshifts for the matter density
-
-        Returns
-        -------
-        Hubble parameter: numpy.ndarray
-            hubble parameter as a function of redshift
-
+    def __init__(self, H0: float, Omb: float, Omc: float, Omk: float, ns: float, As: float, w: float, wa: float, gamma_MG: float):
         """
-        pass
+        A protocol to define background cosmology
+        """
+        ...
 
-    @abstractmethod
-    def comoving_distance(self, zs):
-        r"""
+    def hubble_parameter(self, zs: np.ndarray, units: str = '1/Mpc') -> np.ndarray:
+        """
+        Retrieves the hubble parameter as a function of redshift.
+        """
+        ...
+
+    def comoving_distance(self, zs: np.ndarray) -> np.ndarray:
+        """
         Calculates the comoving distance for given redshifts.
-
-        Parameters:
-        -----------
-        zs : array_like
-            Redshifts at which to calculate the comoving distance.
-
-        Returns:
-        --------
-        np.ndarray
-            The comoving distance as a function of redshift.
         """
-        pass
+        ...
 
-    @abstractmethod
-    def transverse_comoving_distance(self, zs) -> np.ndarray:
+    def transverse_comoving_distance(self, zs: np.ndarray) -> np.ndarray:
         """
         Calculates the transverse comoving distance for given redshifts.
-
-        Parameters:
-        -----------
-        zs : array_like
-            Redshifts at which to calculate the transverse comoving distance.
-
-        Returns:
-        --------
-        np.ndarray
-            The transverse comoving distance as a function of redshift.
         """
-        return
+        ...
 
-    @abstractmethod
-    def angular_diameter_distance(self, zs) -> np.ndarray:
+    def angular_diameter_distance(self, zs: np.ndarray) -> np.ndarray:
         """
         Calculates the angular diameter distance for given redshifts.
-
-        Parameters:
-        -----------
-        zs : array_like
-            Redshifts at which to calculate the angular diameter distance.
-
-        Returns:
-        --------
-        np.ndarray
-            The angular diameter distance as a function of redshift.
         """
-        return
+        ...
 
-    @abstractmethod
-    def matter_density(self, zs) -> np.ndarray:
-        r"""
-        Computes the matter density as
-
-        .. math::
-            \Omega_{\rm m}(z) = \Omega_{{\rm m},0}(1+z)^3H_0^2/H^2(z)
-
-        Parameters
-        ----------
-        zs: numpy.ndarray
-            Redshifts at which to calculate the matter density
-
-        Returns
-        -------
-        Matter density parameter: numpy.ndarray
-            Matter density as a function of redshift
-
+    def matter_density(self, zs: np.ndarray) -> np.ndarray:
         """
+        Computes the matter density as a function of redshift.
+        """
+        ...
 
-        return
 
-class LinearPerturbations(ABC):
-    def __init__(self, background : Background):
-        self.background = background
+class LinearPerturbations(Protocol):
+    background: Background
 
-    @abstractmethod
-    def growth_factor(self, zs, ks) -> np.ndarray:
+    def growth_factor(self, zs: np.ndarray, ks: np.ndarray) -> np.ndarray:
         """
         Calculates the growth factor for given redshifts and wavenumbers.
-
-        Parameters:
-        -----------
-        zs : array_like
-            Redshifts at which to calculate the growth factor.
-        ks : array_like
-            Wavenumbers at which to calculate the growth factor.
-
-        Returns:
-        --------
-        np.ndarray
-            The growth factor as a function of redshift and wavenumber.
         """
-        return
+        ...
 
-    @abstractmethod
-    def growth_rate(self, zs, ks) -> np.ndarray:
+    def growth_rate(self, zs: np.ndarray, ks: np.ndarray) -> np.ndarray:
         """
         Calculates the growth rate for given redshifts and wavenumbers.
-
-        Parameters:
-        -----------
-        zs : array_like
-            Redshifts at which to calculate the growth rate.
-        ks : array_like
-            Wavenumbers at which to calculate the growth rate.
-
-        Returns:
-        --------
-        np.ndarray
-            The growth rate as a function of redshift and wavenumber.
         """
-        return
+        ...
 
-    @abstractmethod
-    def linear_matter_power_spectrum(self):
-        r"""
-        Retrieves the hubble parameter as
-        a function of redshift
-
-        .. math::
-            H(z) = \sqrt
-
-        Parameters
-        ----------
-        zs: numpy.ndarray
-            Redshifts for the matter density
-
-        Returns
-        -------
-        Hubble parameter: numpy.ndarray
-            hubble parameter as a function of redshift
-
+    def linear_matter_power_spectrum(self) -> np.ndarray:
         """
-    pass
-
-class NonLinearPerturbations(ABC):
-    def __init__(self, linearperturbations : LinearPerturbations):
-        self.linearperturbations = linearperturbations
-
-    @abstractmethod
-    def nonlinear_matter_power_spectrum(self):
-        r"""
-        Retrieves the hubble parameter as
-        a function of redshift
-
-        .. math::
-            H(z) = \sqrt
-
-        Parameters
-        ----------
-        zs: numpy.ndarray
-            Redshifts for the matter density
-
-        Returns
-        -------
-        Hubble parameter: numpy.ndarray
-            hubble parameter as a function of redshift
-
+        Retrieves the linear matter power spectrum.
         """
-    pass
+        ...
 
+class NonLinearPerturbations(Protocol):
+    linear_perturbation: LinearPerturbations
 
-class Cosmology:
-    """
-    A general parent class that collects cosmological background quantities
-    and the linear matter power spectrum from different backends.
-
-    CAMB
-    CLASS (work-in-progress)
-    JAX
-    general emulator (work-in-progress)
-
-    """
-
-    def __init__(self, H0: float, Omb: float, Omc: float, Omk: float, As: float, ns: float,
-                 w: float, wa: float, gamma_MG: float, background_backend: str,
-                 perturbations_backend: str):
+    def growth_factor(self, zs: np.ndarray, ks: np.ndarray) -> np.ndarray:
         """
-        Initializes the CosmologyParameters class with the provided parameters.
-
-        Parameters:
-        -----------
-        H0 : float
-            Present-day Hubble constant
-            :math:`{\rm (km·s^{-1}·Mpc^{-1})}`
-        Omb : float
-            Present-day baryon energy density
-            :math:`\Omega_{\rm baryon}`
-        Omc : float
-            Present-day CDM energy density
-            :math:`\Omega_{\rm CDM}`
-        Omk : float
-            Present-day curvature energy density
-            :math:`\Omega_{\rm k}`
-        As : float
-            Amplitude of the primordial power spectrum
-            :math:`A_{\rm s}`
-        ns : float
-            Spectral tilt of the primordial power spectrum
-            :math:`n_{\rm s}`
-        w : float
-            Dark energy equation of state parameter
-            :math:`w_0`
-        wa : float
-            Dark energy equation of state parameter
-            :math:`w_a`
-        gamma_MG: float
-           Linder Modified Gravity parameter
-           :math:`\gamma_{\rm g}`
-        backend : str
-            Selected backend to calculate cosmological quantities.
-            Choose between: CAMB, JAX
+        Calculates the growth factor for given redshifts and wavenumbers.
         """
-        # Ensure the attributes are floats
-        self.H0 = float(H0)
-        self.Omb = float(Omb)
-        self.Omc = float(Omc)
-        self.Omk = float(Omk)
-        self.As = float(As)
-        self.ns = float(ns)
-        self.w = float(w)
-        self.wa = float(wa)
-        self.gamma_MG = float(gamma_MG)
-        self.backend = {"background": background_backend,
-                        "perturbations": perturbations_backend}
+        ...
 
-        # Dynamically instantiate the appropriate child class based on the backend
-        if self.backend["background"] == 'CAMB':
-            from camb_cosmology import CAMBBackground
-            self.background_backend = CAMBBackground(H0, Omb, Omc, Omk, As, ns, w, wa, gamma_MG)
-        elif self.backend["background"] == 'JAX':
-            from cloelite.cosmology.jax_cosmology import JAXBackground
-            self.background_backend = JAXBackground(H0, Omb, Omc, Omk, As, ns, w, wa, gamma_MG)
-        else:
-            raise ValueError(f"Unsupported background backend: {background_backend}. Choose between: CAMB, JAX")
+    def growth_rate(self, zs: np.ndarray, ks: np.ndarray) -> np.ndarray:
+        """
+        Calculates the growth rate for given redshifts and wavenumbers.
+        """
+        ...
 
-        if self.backend["perturbations"] == 'CAMB':
-            from camb_cosmology import CAMBPerturbations
-            self.perturbations_backend = CAMBPerturbations(self.background_backend)
-        elif self.backend["perturbations"] == 'JAX':
-            from cloelite.cosmology.jax_cosmology import JAXPerturbations
-            self.perturbations_backend = JAXPerturbations(self.background_backend)
-        else:
-            raise ValueError(f"Unsupported perturbations backend: {perturbations_backend}. Choose between: CAMB, JAX")
+    def nonlinear_matter_power_spectrum(self) -> np.ndarray:
+        """
+        Retrieves the linear matter power spectrum.
+        """
+        ...

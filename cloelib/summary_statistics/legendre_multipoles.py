3ac7ee063ea1cff13997da9189fd9ff78df99a05
AndreaPezzotta <andryp89@gmail.com>
2025-03-16 18:56:37 +0100

    gh-66: Comet restored with protocols (#93)
    Co-authored-by: Andrea Pezzotta <pezzotta@op09lm.local>
    Reviewed by: Guadalupe Cañas-Herrera

--- cloelib/summary_statistics/legendre_multipoles.py
+++ cloelib/summary_statistics/legendre_multipoles.py
@@ -1,34 +1,55 @@
 # cloelib imports
+from cloelib.cosmology.cosmology import Background
 from cloelib.observables.spectro import SpectroPower
-from cloelib.auxiliary.math_utils import legendre, simps_jax # I made it auto-diff :) 
+from cloelib.auxiliary.math_utils import legendre, simps_jax
 
 # General imports
 from typing import Protocol, Union, TypeVar, Optional, Generic
-import numpy as np  # type: ignore
-import jax.numpy as jnp # type: ignore
-from scipy import integrate # type: ignore
-from scipy.special import roots_legendre # type: ignore
+import numpy as np
+import jax.numpy as jnp
+from scipy import integrate
+from scipy.special import roots_legendre
 
-"""
 
-## Notes:
+class LegendreMultipoles:
+    r"""Class to compute spectroscopic Legendre multipoles of the galaxy
+    power spectrum using an external non-linear code
 
-- Class to calculate the legendre multiples given a SpectroPower instance
+    Parameters
+    ----------
+    spectro_power: SpectroPower
+        Class returning the anisotropic power spectrum (only density and
+        velocity field couplings; noise and systematics are included directly
+        here)
+    background_fiducial: Background
+        Background class for computing fiducial background distances
+    parameters: dict
+        Dictionary containing shot noise and parameters related to
+        observational systematics
+    nbar: float
+        Mean number denisty of the sample
+    """
 
-"""
-
-class LegendreMultipoles: #this class depends on SpectroPower
     def __init__(self, spectro_power: SpectroPower,
-                 nbar: float, sigmaz: float, fout: float, redshift: float):
+                 background_fiducial: Background,
+                 parameters: dict,
+                 nbar: float):
+        r"""Class constructor
+        """
+
         self.spectro_power = spectro_power
+        self.redshift = spectro_power.redshift
+
+        self.background_fiducial = background_fiducial
+
         mu_min = 0.0
         mu_max = 1.0
         mu_samp = 101
         self.mu_grid = np.linspace(mu_min, mu_max, mu_samp)
+
+        self.parameters = parameters
         self.nbar = nbar
-        self.sigmaz = sigmaz
-        self.fout = fout
-        self.redshift = redshift
+
 
     def _q_AP_tr(self, zs: np.ndarray) -> np.ndarray:
         r"""AP distortion parameter transversal to the line of sight
@@ -43,11 +64,8 @@ class LegendreMultipoles: #this class depends on SpectroPower
         q_tr: np.ndarray
            Transversal AP parameter
         """
-        # as long as SpectroPower has the attribute background and background_fiducial
-        # this will always work, because it is dependent on Background protocol that will
-        # always have angular_diameter_distance
         return (self.spectro_power.background.angular_diameter_distance(zs)
-                / self.spectro_power.background_fiducial.angular_diameter_distance(zs))
+                / self.background_fiducial.angular_diameter_distance(zs))
 
     def _q_AP_lo(self, zs: np.ndarray) -> np.ndarray:
         r"""AP distortion parameter parallel to the line of sight
@@ -62,7 +80,7 @@ class LegendreMultipoles: #this class depends on SpectroPower
         q_tr: np.ndarray
            Parallel AP parameter
         """
-        return (self.spectro_power.background_fiducial.hubble_parameter(zs)
+        return (self.background_fiducial.hubble_parameter(zs)
                 /self.spectro_power.background.hubble_parameter(zs))
 
     def _ensure_array(self, param):
@@ -120,29 +138,7 @@ class LegendreMultipoles: #this class depends on SpectroPower
         q_lo = self._q_AP_lo(zs) if use_AP else 1.0
         return mu / q_lo / np.sqrt(mu**2 / q_lo**2 + (1.0-mu**2) / q_tr**2)
 
-    def Pk2d_noise(self, k: np.ndarray, mu: np.ndarray,
-                    parameters: dict) -> np.ndarray:
-        r"""2D power spectrum from expansion of stochastic field
-        Parameters
-        ----------
-        k: np.ndarray
-            Wavenumber
-        mu: np.ndarray
-            Angle (cosinus) to the line of sight
-        parameters: dict
-            Ensemble of cosmological and nuisance parameters
-        Returns
-        -------
-        Pk2d_noise: np.ndarray
-            2D power spectrum from expansion of stochastic field
-        """
-        noise = parameters['NP0'] + k**2 * (parameters['NP20'] +
-                                            parameters['NP22'] *
-                                            legendre(2, mu))
-        return noise / self.nbar
-
-    def _damping_function(self, k: np.ndarray, mu: np.ndarray,
-                          parameters: dict) -> np.ndarray:
+    def _damping_function(self, k: np.ndarray, mu: np.ndarray) -> np.ndarray:
         r"""Damping function due to GCsp redshift uncertainty
         Parameters
         ----------
@@ -150,20 +146,17 @@ class LegendreMultipoles: #this class depends on SpectroPower
             Wavenumber
         mu: np.ndarray
             Angle (cosinus) to the line of sight
-        parameters: dict
-            Ensemble of cosmological and nuisance parameters
         Returns
         -------
         damping_function: np.ndarray
             Damping function due to GCsp redshift uncertainty
         """
-        sigma_z = parameters['sigmaz']
+        sigma_z = self.parameters['sigmaz']
         sigma_r = 299792.458 * sigma_z / \
-            self.spectro_power.background_fiducial.hubble_parameter(parameters['z'])
+            self.background_fiducial.hubble_parameter(self.redshift)
         return np.exp(-k**2 * mu**2 * sigma_r**2)
 
-    def _Pk2d_noise(self, k: np.ndarray, mu: np.ndarray,
-                    parameters: dict) -> np.ndarray:
+    def _Pk2d_noise(self, k: np.ndarray, mu: np.ndarray) -> np.ndarray:
         r"""2D power spectrum from expansion of stochastic field
         Parameters
         ----------
@@ -171,20 +164,17 @@ class LegendreMultipoles: #this class depends on SpectroPower
             Wavenumber
         mu: np.ndarray
             Angle (cosinus) to the line of sight
-        parameters: dict
-            Ensemble of cosmological and nuisance parameters
         Returns
         -------
         Pk2d_noise: np.ndarray
             2D power spectrum from expansion of stochastic field
         """
-        noise = parameters['NP0'] + k**2 * (parameters['NP20'] +
-                                            parameters['NP22'] *
-                                            legendre(2, mu))
+        noise = self.parameters['NP0'] + k**2 * (self.parameters['NP20'] +
+                                                 self.parameters['NP22'] *
+                                                 legendre(2, mu))
         return noise / self.nbar
 
-    def _Pk2d_tot(self, k: np.ndarray, mu: np.ndarray,
-                  parameters: dict) -> np.ndarray:
+    def _Pk2d_tot(self, k: np.ndarray, mu: np.ndarray) -> np.ndarray:
         r"""Total 2D power spectrum (including RSD, systematics, and noise)
         Parameters
         ----------
@@ -192,23 +182,17 @@ class LegendreMultipoles: #this class depends on SpectroPower
             Wavenumber
         mu: np.ndarray
             Angle (cosinus) to the line of sight
-        parameters: dict
-            Ensemble of cosmological and nuisance parameters
         Returns
         -------
         Pk2d_tot: np.ndarray
             Total 2D power spectrum (including RSD, systematics, and noise)
         """
-        # here is where you see how to call the SpectroPower method
-        # if the instance you are passing follows the SpectroPower protocol
-        # this will work
-
         return (self.spectro_power.Pk2d_rsd(k, mu) *
-                self._damping_function(k, mu, parameters) *
-                (1.0 - parameters['fout'])**2 +
-                self._Pk2d_noise(k, mu, parameters))
+                self._damping_function(k, mu) *
+                (1.0 - self.parameters['fout'])**2 +
+                self._Pk2d_noise(k, mu))
 
-    def power_multipoles(self, k: np.ndarray, parameters: dict, #not sure about dicts
+    def power_multipoles(self, k: np.ndarray,
                          ells: Optional[np.ndarray] = None,
                          use_AP: Optional[bool] = True) -> dict:
         r"""Power spectrum Legendre multipoles
@@ -216,8 +200,6 @@ class LegendreMultipoles: #this class depends on SpectroPower
         ----------
         k: np.ndarray
             Wavenumber
-        parameters: dict
-            Ensemble of cosmological and nuisance parameters
         ells: np.ndarray
             Legendre multipole order
         use_AP: bool
@@ -227,28 +209,26 @@ class LegendreMultipoles: #this class depends on SpectroPower
         multipoles: dict
             Power spectrum Legendre multipoles
         """
-        #self.update(**parameters) #you don't need update, in reality
-        params = parameters.copy()
         ells = self._ensure_array(ells) if ells else np.array([0,2,4])
-        AP_factor = (self._q_AP_tr(parameters['z'])**2 *
-                     self._q_AP_lo(parameters['z']) if use_AP else 1.0)
+        AP_factor = (self._q_AP_tr(self.redshift)**2 *
+                     self._q_AP_lo(self.redshift) if use_AP else 1.0)
         prefactors = np.array([(2.0 * m + 1.0) for m in ells]) / 2.0 / \
             AP_factor
         multipoles = {}
         for i,ell in enumerate(ells):
             multipoles[f'ell{ell}'] = \
                 integrate.simps(self._Pk2d_tot(self._k_AP(k, self.mu_grid,
-                                                          parameters['z'],
+                                                          self.redshift,
                                                           use_AP=use_AP),
                                                self._mu_AP(self.mu_grid,
-                                                           parameters['z'],
-                                                           use_AP=use_AP),
-                                               params) *
+                                                           self.redshift,
+                                                           use_AP=use_AP)) *
                                 legendre(ell, self.mu_grid),
                                 self.mu_grid, axis=1)
             multipoles[f'ell{ell}'] *= (2.0 * prefactors[i])
         return multipoles
 
+    #This need to be restored; not working at the moment
     def convolved_power_multipoles(self, parameters: dict, mixing_matrix=dict):
         r"""Power spectrum Legendre multipoles convolved with the mixing matrix
         Parameters

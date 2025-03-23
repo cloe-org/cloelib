db2329ddcd63e486b40df5133d6a6e39deca55c4
Santiago Casas <santiagocasas@gmail.com>
2025-03-24 00:47:36 +0100

    rewrite CLASS implementation exactly like CAMB

--- cloelib/cosmology/class_cosmology.py
+++ cloelib/cosmology/class_cosmology.py
@@ -21,7 +21,7 @@ class CLASSBackground:
     """
     c0 = SPEED_OF_LIGHT/1000
     def __init__(self, H0: float, Omega_b0: float, Omega_cdm0: float, Omega_k0: float,
-                 As: float, ns: float,
+                 As: float, ns: float, mnu: float, 
                  w0: float, wa: float, gamma_MG: float) -> None:
         """
         Initializes the CLASSBackground class with cosmological parameters.
@@ -33,6 +33,7 @@ class CLASSBackground:
             Omega_k0 (float): Curvature density parameter.
             As (float): Scalar amplitude of primordial fluctuations.
             ns (float): Scalar spectral index.
+            mnu (float): Sum of neutrino masses in [eV].
             w0 (float): Equation of state parameter for dark energy.
             wa (float): Time evolution of the equation of state.
             gamma_MG (float): Modified gravity growth parameter (not directly used in CLASS, but kept for protocol compliance).
@@ -47,6 +48,7 @@ class CLASSBackground:
         self.w0 = w0
         self.wa = wa
         self.gamma_MG = gamma_MG  # Kept for protocol, but CLASS doesn't directly use it
+        self.mnu = mnu
 
         # Initialize CLASS parameters
         self.interface_args = {'CLASSparams': {}}  # Use a dictionary for CLASS parameters
@@ -55,6 +57,7 @@ class CLASSBackground:
         self.interface_args['CLASSparams']['omega_cdm'] = self.Omega_cdm0 * (self.h)**2
         self.interface_args['CLASSparams']['Omega_k'] = self.Omega_k0
         self.interface_args['CLASSparams']['n_s'] = self.ns
+        self.interface_args['CLASSparams']['m_ncdm'] = self.mnu
         self.interface_args['CLASSparams']['A_s'] = self.As
         self.interface_args['CLASSparams']['w0_fld'] = self.w0 # or w0
         self.interface_args['CLASSparams']['wa_fld'] = self.wa # or wa
@@ -62,6 +65,7 @@ class CLASSBackground:
         self.interface_args['CLASSparams']['use_ppf'] = "yes"
         # To avoid using a cosmological constant
         self.interface_args['CLASSparams']['Omega_Lambda'] = 0. 
+        self.interface_args['CLASSparams']['N_ncdm'] = 1 
 
         # Initialize CLASS
         self.results = Class()
@@ -148,6 +152,18 @@ class CLASSBackground:
             np.ndarray: Matter density values.
         """
         return np.array([self.results.Om_m(z) for z in zs])
+    
+    def Omega_b(self, zs: np.ndarray) -> np.ndarray:
+        """
+        Returns the baryon density as a function of redshift.
+
+        Args:
+            zs (np.ndarray): Array of redshifts.
+
+        Returns:
+            np.ndarray: Matter density values.
+        """
+        return np.array([self.results.Om_b(z) for z in zs])
 
 class CLASSLinearPerturbations:
     def __init__(self, background : Background, redshifts: np.ndarray):
@@ -163,9 +179,11 @@ class CLASSLinearPerturbations:
         # Ensure CLASS is initialized with necessary parameters
         self.interface_args = copy.deepcopy(self.background._interface_args)
         self.interface_args['CLASSparams']['output'] = 'mPk, mTk'
-        self.interface_args['CLASSparams']['P_k_max_1/Mpc'] = 100
+        self.interface_args['CLASSparams']['P_k_max_1/Mpc'] = self.kmax
         self.interface_args['CLASSparams']['k_per_decade_for_bao'] = 70
         self.interface_args['CLASSparams']['k_per_decade_for_pk'] = 10
+        self.interface_args['CLASSparams']['z_max_pk'] = np.max(self.z)
+        self.interface_args['CLASSparams']['non linear'] = "none"
         self.interface_args['CLASSparams']['z_max_pk'] = np.max(self.z)
         self.results = Class()
         self.results.set(self.interface_args['CLASSparams'])
@@ -178,21 +196,37 @@ class CLASSLinearPerturbations:
         """
         return self.interface_args
     
-    def matter_power_spectrum(self) -> np.ndarray:
-        """
-        Calculates the CLASS linear matter power spectrum.
+    def matter_power_spectrum(self, zs, ks, hubble_units=False,
+                              k_hunit=False) -> np.ndarray:
+        """Calculates the CLASS linear matter power spectrum.
+        
+        Parameters
+        ----------
+        zs: numpy.ndarray
+            redshifts
 
-        Returns:
-            np.ndarray: Linear power spectrum values \(P(k)\).
+        ks: numpy.ndarray
+            wavenumber
+
+        hubble_units: (Optional) bool
+            Flag to specify if output in h units, defaults to False
+
+        k_hunit: (Optional) bool
+            Flag to specify if wavenumber in h units, defaults to False
+
+        Returns
+        -------
+        pk: numpy.ndarray
+            Linear matter power spectrum at the specified scale
+            and redshift
         """
-        Pk_l, k_values, z_values = self.results.get_pk_and_k_and_z(nonlinear=False)
-        self.k = k_values
-        self.z = z_values[::-1]
-        self.Pk_linear = (Pk_l.T)[::-1, :]
+        if hubble_units == True or k_hunit == True:
+            raise ValueError("This CLASS method does not yet support h-units")
+        self.Pk_linear = np.array([[self.results.pk(ki, zi) for ki in ks] for zi in zs])
         # To match array convention of CAMB
         return self.Pk_linear
 
-    def growth_factor(self) -> np.ndarray:
+    def growth_factor(self, zs, ks) -> np.ndarray:
         """
         Calculates the growth factor for given redshifts and wavenumbers.
 
@@ -201,71 +235,39 @@ class CLASSLinearPerturbations:
             /P_{\rm \delta\delta}(z=0, k)}\\
 
         and normalizes as for :math:`D(z)/D(0)`.
+        
+        Parameters
+        ----------
+        zs: numpy.ndarray
+            redshifts
+
+        ks: numpy.ndarray
+            wavenumber
 
         Returns:
         --------
         np.ndarray
-            The growth factor as a function of redshift and wavenumber.
+            The growth factor at the specified redshift and wavenumber.
         """
-        if hasattr(self, 'Pk_linear') and self.Pk_linear is not None:
-            self.D_z_k = np.sqrt(self.Pk_linear / self.Pk_linear[0, :])
-        else:
-            self.matter_power_spectrum()
-            self.D_z_k = np.sqrt(self.Pk_linear / self.Pk_linear[0, :])
-        return self.D_z_k
-    
-    def f_deriv(self, k_fix=True, fixed_k=1e-3) -> np.ndarray:
-        """
-        Calculate the growth rate f(z,k).
+        D_z_k = np.sqrt(self.matter_power_spectrum(zs, ks) / \
+                        self.matter_power_spectrum(np.zeros_like(zs), ks))
 
-        Parameters
-        ----------
-        k_fix : bool, optional
-            If True, use a fixed k value (default is False).
-        fixed_k : float, optional
-            Fixed k value to use if k_fix is True (default is 1e-3).
-
-        Returns
-        -------
-        tuple
-            Growth rate f(z,k) and the corresponding z array.
-        """
-        if not hasattr(self, 'D_z_k') or self.D_z_k is None:
-            self.growth_factor()
-        if k_fix:
-            k_array = np.full((len(self.k)), fixed_k)
-        ## Generates interpolators D(z) for varying k values
-        D_zk_interp = np.array([UnivariateSpline(self.z, self.D_z_k[:, ki], s=0) for ki in range(len(k_array))])
-        ## Generates arrays f(z) for varying k values
-        self.f_zk = np.array(
-            [-(1 + self.z) / D_z(self.z) * (D_z.derivative())(self.z) for D_z in D_zk_interp]
-        )
-        return self.f_zk
+        return D_z_k
     
-    def growth_rate(self, k_fix=True) -> np.ndarray:
+    def growth_rate(self) -> np.ndarray:
         """
-        Calculates the growth rate f(z,k).
+        Calculates the growth rate f(z).
 
         Returns
         -------
         np.ndarray
-            If k_fix is True, returns the growth rate as a 1D array over redshift.
-            Otherwise, returns the full 2D growth rate array.
+            Scale-independent growth rate f(z)
         """
-        # Ensure that the growth rate has been computed
-        if not hasattr(self, 'f_zk'):
-            self.f_deriv(k_fix=k_fix)
-
-        if k_fix:
-            # Return the redshift part (the first row), since all k's are equal by construction
-            return self.f_zk[0, :]
-        else:
-            # Return the full 2D array (varying over z and k)
-            return self.f_zk
+        return [self.results.scale_independent_growth_factor_f(zi)
+                for zi in self.z]
 
 class CLASSNonLinearPerturbations:
     def __init__(self, background : Background, 
-                 linear_perturbations: CLASSLinearPerturbations, 
                  redshifts: np.ndarray,
                  nonlinear_model: Optional[str] = None):
         r"""
@@ -273,13 +275,16 @@ class CLASSNonLinearPerturbations:
         and inheriting from Perturbations parent class
         """
         self.background = background
-        self.linear_perturbations = linear_perturbations
         self.z = redshifts
         self.kmax = 100
-        self.results = linear_perturbations.results  # Store CLASS results
 
         # Ensure CLASS is initialized with necessary parameters
-        self.interface_args = copy.deepcopy(self.linear_perturbations._interface_args)
+        self.interface_args = copy.deepcopy(self.background._interface_args)
+        self.interface_args['CLASSparams']['output'] = 'mPk, mTk'
+        self.interface_args['CLASSparams']['P_k_max_1/Mpc'] = self.kmax
+        self.interface_args['CLASSparams']['k_per_decade_for_bao'] = 70
+        self.interface_args['CLASSparams']['k_per_decade_for_pk'] = 10
+        self.interface_args['CLASSparams']['z_max_pk'] = np.max(self.z)
         self.interface_args['CLASSparams']['nonlinear_min_k_max'] = 50
         self.interface_args['CLASSparams']['hmcode_tol_sigma'] = 1e-8
         self.interface_args['CLASSparams']['non linear'] = nonlinear_model
@@ -288,29 +293,72 @@ class CLASSNonLinearPerturbations:
         self.results.set(self.interface_args['CLASSparams'])
         self.results.compute()
 
-    def matter_power_spectrum(self) -> np.ndarray:
+    def matter_power_spectrum(self, zs, ks, hubble_units=False,
+                              k_hunit=False) -> np.ndarray:
+        """Calculates the CLASS non-linear matter power spectrum.
+        
+        Parameters
+        ----------
+        zs: numpy.ndarray
+            redshifts
+
+        ks: numpy.ndarray
+            wavenumber
+
+        hubble_units: (Optional) bool
+            Flag to specify if output in h units, defaults to False
+
+        k_hunit: (Optional) bool
+            Flag to specify if wavenumber in h units, defaults to False
+
+        Returns
+        -------
+        pk: numpy.ndarray
+            Non-linear matter power spectrum at the specified scale
+            and redshift
         """
-        Calculates the CLASS linear matter power spectrum.
+        if hubble_units == True or k_hunit == True:
+            raise ValueError("This CLASS method does not yet support h-units")
+        self.Pk_nonlinear = np.array([[self.results.pk(ki, zi) for ki in ks] for zi in zs])
+        # To match array convention of CAMB
+        return self.Pk_nonlinear
+
+    def growth_factor(self, zs, ks) -> np.ndarray:
+        """
+        Calculates the growth factor for given redshifts and wavenumbers.
+
+        .. math::
+            D(z, k) =\sqrt{P_{\rm \delta\delta}(z, k)\
+            /P_{\rm \delta\delta}(z=0, k)}\\
+
+        and normalizes as for :math:`D(z)/D(0)`.
+        
+        Parameters
+        ----------
+        zs: numpy.ndarray
+            redshifts
+
+        ks: numpy.ndarray
+            wavenumber
 
         Returns:
-            np.ndarray: Linear power spectrum values \(P(k)\).
+        --------
+        np.ndarray
+            The growth factor at the specified redshift and wavenumber.
         """
-        Pk_nl, k_values, z_values = self.results.get_pk_and_k_and_z(nonlinear=True)
-        self.k = k_values
-        self.z = z_values[::-1]
-        self.Pk_nonlinear = (Pk_nl.T)[::-1, :]
-        return self.Pk_nonlinear
+        D_z_k = np.sqrt(self.matter_power_spectrum(zs, ks) / \
+                        self.matter_power_spectrum(0.0, ks))
+
+        return D_z_k
     
-    def growth_factor(self) -> np.ndarray:
+    def growth_rate(self) -> np.ndarray:
         """
-        Calculates growth factor from linear perturbations
-        This method is here to follow Protocol definition.
-        """ 
-        return self.linear_perturbations.growth_factor()
-    
-    def growth_rate(self, k_fix=True) -> np.ndarray:
+        Calculates the growth rate f(z).
+
+        Returns
+        -------
+        np.ndarray
+            Scale-independent growth rate f(z)
         """
-        Calculates growth rate from linear perturbations
-        This method is here to follow Protocol definition.
-        """ 
-        return self.linear_perturbations.growth_rate(k_fix=k_fix)
+        return [self.results.scale_independent_growth_factor_f(zi)
+                for zi in self.z]

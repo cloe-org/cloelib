e98ae292f77b36fb7fb3acfb4ccfd364fda9e083
AndreaPezzotta <andryp89@gmail.com>
2025-04-01 19:13:31 +0200

    gh-113: Multiplication_with_mixing_matrix_for_GCspectro (#115)
    Co-authored-by: Andrea Pezzotta <pezzotta@op09lm.local>

--- cloelib/summary_statistics/legendre_multipoles.py
+++ cloelib/summary_statistics/legendre_multipoles.py
@@ -209,7 +209,7 @@ class LegendreMultipoles:
         multipoles: dict
             Power spectrum Legendre multipoles
         """
-        ells = self._ensure_array(ells) if ells else np.array([0,2,4])
+        ells = self._ensure_array(ells) if ells is not None else np.array([0,2,4])
         AP_factor = (self._q_AP_tr(self.redshift)**2 *
                      self._q_AP_lo(self.redshift) if use_AP else 1.0)
         prefactors = np.array([(2.0 * m + 1.0) for m in ells]) / 2.0 / \
@@ -228,52 +228,43 @@ class LegendreMultipoles:
             multipoles[f'ell{ell}'] *= (2.0 * prefactors[i])
         return multipoles
 
-    def convolved_power_multipoles(self, parameters: dict, mixing_matrix=dict):
+    def convolved_power_multipoles(self, mixing_matrix: dict,
+                                   ells: Optional[np.ndarray] = None,
+                                   use_AP: Optional[bool] = True) -> dict:
         r"""Power spectrum Legendre multipoles convolved with the mixing matrix
         Parameters
         ----------
-        parameters: dict
-            Ensemble of cosmological and nuisance parameters
+        mixing_matrix: dict
+            Dicitonary containing the mixing matrix
+        ells: np.ndarray
+            Legendre multipole order
+        use_AP: bool
+            Flag to switch between with and without AP corrections
         Returns
         -------
         multipoles_out: dict
             Convolved power spectrum Legendre multipoles
         """
+        ells_tot = [0, 2, 4]
+        ells = self._ensure_array(ells) if ells is not None else ells_tot
 
-        self.mixing_matrix_dict = mixing_matrix
+        kin_arrays = [mixing_matrix[f'kin{ell}'] for ell in ells_tot]
 
-        for key in self.mixing_matrix_dict:
-            self.mixing_matrix_dict[key] = \
-                np.asarray(self.mixing_matrix_dict[key], dtype=np.float32)
-
-        kin0 = self.mixing_matrix_dict['kin0']
-        kin2 = self.mixing_matrix_dict['kin2']
-        kin4 = self.mixing_matrix_dict['kin4']
-        kout = self.mixing_matrix_dict['kout']
-
-        multipoles_in = {}
-        if np.all(kin0 == kin2) and np.all(kin2 == kin4):
-            multipoles_in = self.power_multipoles(k=kin0,
-                                                  parameters=parameters,
-                                                  ells=[0,2,4])
+        if all(np.array_equal(kin_arrays[0], kin) for kin in kin_arrays):
+            multipoles_in = self.power_multipoles(
+                k=kin_arrays[0], ells=ells_tot, use_AP=use_AP)
         else:
-            for ell in [0,2,4]:
-                multipoles_in[f'ell{ell}'] = \
-                    self.power_multipoles(
-                        k=self.mixing_matrix_dict[f'kin{ell}'],
-                        parameters=parameters, ells=[ell])
-
-        for key in multipoles_in:
-            multipoles_in[key] = \
-                np.asarray(multipoles_in[key], dtype=np.float32)
+            multipoles_in = {
+                f'ell{ell}': self.power_multipoles(k=kin_arrays[i], ells=[ell],
+                                                   use_AP=use_AP)
+                for i, ell in enumerate(ells_tot)}
 
         multipoles_out = {}
-        multipoles_out['k'] = kout
-        for ell in [0, 2, 4]:
-            multipoles_out[f'ell{ell}'] = np.zeros(kout.shape)
-            for ell_prime in [0, 2, 4]:
-                multipoles_out[f'ell{ell}'] += \
-                    np.dot(self.mixing_matrix_dict[f'W{ell}{ell_prime}'],
+        multipoles_out['k'] = mixing_matrix['kout']
+        for ell in ells:
+            multipoles_out[f'ell{ell}'] = (
+                sum(np.dot(mixing_matrix[f'W{ell}{ell_prime}'],
                            multipoles_in[f'ell{ell_prime}'])
+                    for ell_prime in ells_tot))
 
         return multipoles_out

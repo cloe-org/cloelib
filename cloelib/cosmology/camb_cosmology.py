cd59cf9bb31471878794d4c7dce0f6d2c42a22e9
Guadalupe Canas-Herrera <guadalupe.canasherrera@esa.int>
2025-04-07 10:59:28 +0200

    Revert "Merge pull request #124 from cloe-org/123-fix-camb-class-cosmo"
    This reverts commit 0196d1b1b1961417896aff0db055c6d370b35767, reversing
    changes made to a05cf0f48993ddeb928f1cf218194e7a4fbd9871.

--- cloelib/cosmology/camb_cosmology.py
+++ cloelib/cosmology/camb_cosmology.py
@@ -70,13 +70,6 @@ class CAMBBackground:
 
         # Call CAMB to compute the background
         self.results = camb.get_background(self.interface_args['CAMBparams'])
-    
-    @property
-    def _interface_args(self) -> dict:
-        """
-        Save internal structure format of interface codes
-        """
-        return self.interface_args
 
     def hubble_parameter(self, zs: np.ndarray, units: str = "km/s/Mpc") -> np.ndarray:
         """
@@ -117,7 +110,9 @@ class CAMBBackground:
         Returns:
             np.ndarray: Transverse comoving distance values.
         """
-        x = self.comoving_distance(zs)
+        c_0 = SPEED_OF_LIGHT / 1000  # Convert to km/s
+        delta_z = self.comoving_distance(zs)[None, :] - self.comoving_distance(zs)[:, None]
+        x = delta_z * self.H0 / c_0
 
         if self.Omega_k0 == 0.0:
             y = x
@@ -126,7 +121,7 @@ class CAMBBackground:
         else:
             y = np.sin(np.sqrt(-self.Omega_k0) * x) / np.sqrt(-self.Omega_k0)
 
-        return y
+        return y * (c_0 / self.H0)
 
     def angular_diameter_distance(self, zs: np.ndarray) -> np.ndarray:
         """

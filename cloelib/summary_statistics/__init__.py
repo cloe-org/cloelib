f88fdab6c1015b5592f665a5df04e779c9749aea
llinke1 <laila.linke@uibk.ac.at>
2025-04-24 16:07:56 +0200

    fixed typo

--- cloelib/summary_statistics/__init__.py
+++ cloelib/summary_statistics/__init__.py
@@ -1,4 +1,4 @@
 from cloelib.summary_statistics.angular_two_point import *
-from cloelib.summary_statistics.legendre_multiples import *
+from cloelib.summary_statistics.legendre_multipoles import *
 from cloelib.summary_statistics.correlation_function import *
 

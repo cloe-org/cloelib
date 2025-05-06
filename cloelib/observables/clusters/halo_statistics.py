d704eb17c6ed201c27bd9befe7b138910a856b5c
Michel Aguena <aguena@inaf.it>
2025-05-06 15:00:04 +0200

    make background a property of HaloStatistics

--- cloelib/observables/clusters/halo_statistics.py
+++ cloelib/observables/clusters/halo_statistics.py
@@ -4,6 +4,11 @@ import jax.numpy as np
 
 
 class HaloStatistics:
+
+    @property
+    def background(self):
+        return self.perturbations.bakcground
+
     def __init__(
         self,
         perturbations: Perturbations,
@@ -15,7 +20,6 @@ class HaloStatistics:
         k_max: float = 1.0e2,
     ):
         self.perturbations = perturbations
-        self.background = perturbations.background
 
         if overdensity_type not in ["crit", "mean", "vir"]:
             raise ValueError("Invalid overdensity definition, %s." % overdensity_type)

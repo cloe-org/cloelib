c0f4bd733f5a078639f6601337726bc47bc308ab
Michel <aguena@lapp.in2p3.fr>
2025-05-07 15:50:53 +0200

    put tests in functions

--- cloelib/notebooks/Cluster_tests.py
+++ cloelib/notebooks/Cluster_tests.py
@@ -11,6 +11,94 @@ from cloelib.observables.clusters.clustering import HaloClustering
 
 import numpy as np
 
+
+def test_selectionfunction(SF):
+    z_test = np.linspace(0.01, 1.0, 20)
+    zob_test = np.linspace(0.1, 1.1, 20)
+    M_test = 1.0e14
+    l_test = np.logspace(0.0, 2.0, 20)
+    lob_test = np.logspace(0.2, 2.2, 20)
+
+    SF.lnlambda(z_test, M_test)
+    SF.scatter_lnl(z_test, M_test)
+    SF.P_lnlbd(z_test, M_test, l_test)
+    SF.scatter_lbdobs_lbd(z_test, l_test)
+    SF.P_lbdobs_lbd(z_test, l_test, lob_test)
+    SF.scatter_zobs_z(lob_test, z_test)
+    SF.P_zobs_z(zob_test, lob_test, z_test)
+
+
+def test_halostatistics(HS, HS_tinker, HS_castro):
+    z_test = np.linspace(0.01, 1.0, 20)
+    k_test = np.logspace(-2, 1, 100)
+    R_test = np.logspace(-1, 1, 20)
+    M_test = np.logspace(14, 15, 50)
+
+    HS.window(k_test, R_test)
+    HS.radius_M(M_test)
+    HS.delta_c(z_test)
+    HS.get_Delta_crit(z_test)
+    HS.sigma_z_R(z_test, R_test)
+    HS.sigma_z_M(z_test, M_test)
+    HS.nu_z_M(z_test, M_test)
+    HS.dlns_dlnR(z_test, M_test)
+
+    # HS_tinker.bias(z_test, M_test)
+
+    HS_castro.dn_dm(z_test, M_test)
+    HS_castro.bias(z_test, M_test)
+
+
+def test_profiles(profile_nfw, profile_bmo):
+    R_test = np.linspace(1e-5, 2, 21)
+    z_test = np.linspace(0.01, 1.0, 20)
+    M_test = 5e14
+    c_test = 4.0
+    z_sources_test = np.linspace(0.5, 1, 21)
+    zbin_test = 1
+
+    for _prof in (profile_nfw, profile_bmo):
+        _prof.sigma_crit(z_test, z_sources_test)
+        _prof.n_zs_norM(z_test)
+        _prof.n_zs(z_test)
+        # _prof.m_sig_crit_m1(z_test, zbin_test)
+        # _prof._surface_mass_density_cen(R_test, z_test, c_test, M_test, force_no_2h=False)
+        # _prof._surface_mass_density_profile(R_test, RDelta_test, c_test, Delta_crit_test, rho_c_test)
+        _prof.surface_mass_density(
+            R_test, z_test[0], c_test, M_test, force_no_2h=False, force_no_off=False
+        )
+        _prof.excess_surface_mass_density(R_test, z_test, c_test, M_test)
+        # _prof._mean_surface_mass_density_profile(R_test, RDelta_test, c_test, Delta_crit_test, rho_c_test)
+        _prof.surface_mass_density_2h(R_test, z_test, M_test)
+        _prof.excess_surface_mass_density_2h(R_test, z_test, M_test)
+        # _prof.F_term(x_test)
+        # _prof.G_term(x_test)
+        # _prof._surface_mass_density_profile(R_test, RDelta_test, c_test, Delta_crit_test, rho_c_test)
+        # _prof._mean_surface_mass_density_profile(R_test, RDelta_test, c_test, Delta_crit_test, rho_c_test)
+
+
+def test_clustering(background_fid, perturbations_fid):
+    k_min = 1e-4
+    k_max = 2e0
+    k_div = 300
+    nonu = True
+    CL = HaloClustering(perturbations, perturbations_fid, nonu, k_div, k_min, k_max)
+
+    z_test = np.linspace(0.0, 2.0, 20)
+    r_test = np.geomspace(20, 150, 30)
+
+    CL.APcorr_func(z_test)
+    CL.WF_ra(z_test, r_test)
+
+    Pk_test = perturbations.matter_power_spectrum(
+        z_test, CL.k, hubble_units=True, k_hunit=True
+    )
+    CL.Pk_IR_func(Pk_test)
+
+    sigma_zob = SF.scatter_zobs_z(lob_test, z_test)
+    CL.photoz_rsd_correction(z_test, sigma_zob)
+
+
 if __name__ == "__main__":
     # Cosmology parameters
     print("# Cosmology parameters")
@@ -72,48 +160,15 @@ if __name__ == "__main__":
         sig_z_z,
         sig_z_lambda,
     )
-
-    z_test = np.linspace(0.01, 1.0, 20)
-    zob_test = np.linspace(0.1, 1.1, 20)
-    M_test = 1.0e14
-    l_test = np.logspace(0.0, 2.0, 20)
-    lob_test = np.logspace(0.2, 2.2, 20)
-
-    SF.lnlambda(z_test, M_test)
-    SF.scatter_lnl(z_test, M_test)
-    SF.P_lnlbd(z_test, M_test, l_test)
-    SF.scatter_lbdobs_lbd(z_test, l_test)
-    SF.P_lbdobs_lbd(z_test, l_test, lob_test)
-    SF.scatter_zobs_z(lob_test, z_test)
-    SF.P_zobs_z(zob_test, lob_test, z_test)
+    test_selectionfunction(SF)
 
     # HaloStatistics
     print("# HaloStatistics")
     HS = HaloStatistics(perturbations, "vir")
-
-    k_test = np.logspace(-2, 1, 100)
-    R_test = np.logspace(-1, 1, 20)
-    M_test = np.logspace(14, 15, 50)
-
-    """
-    HS.window(k_test, R_test)
-    HS.radius_M(M_test)
-    HS.delta_c(z_test)
-    HS.get_Delta_crit(z_test)
-    HS.sigma_z_R(z_test, R_test)
-    HS.sigma_z_M(z_test, M_test)
-    HS.nu_z_M(z_test, M_test)
-    HS.dlns_dlnR(z_test, M_test)
-    """
-
     HS_tinker = HaloStatisticsTinker(perturbations, "vir")
-    # HS_tinker.bias(z_test, M_test)
-
     HS_castro = HaloStatisticsCastro(perturbations, "vir")
-    """
-    HS_castro.dn_dm(z_test, M_test)
-    HS_castro.bias(z_test, M_test)
-    """
+
+    test_halostatistics(HS, HS_tinker, HS_castro)
 
     # Profiles
     print("# Profiles ")
@@ -132,62 +187,26 @@ if __name__ == "__main__":
     profile_nfw = ProfileNFW(HS_castro, **_prof_kwargs)
     profile_bmo = ProfileBMO(HS_castro, **_prof_kwargs)
 
-    R_test = np.linspace(1e-5, 2, 21)
-    # z_test = 0.5
-    M_test = 5e14
-    c_test = 4.0
-    z_sources_test = np.linspace(0.5, 1, 21)
-    zbin_test = 1
-
-
-
-    for _prof in (profile_nfw, profile_bmo):
-        _prof.sigma_crit(z_test, z_sources_test)
-        _prof.n_zs_norM(z_test)
-        _prof.n_zs(z_test)
-        _prof.m_sig_crit_m1(z_test, zbin_test)
-        # _prof._surface_mass_density_cen(R_test, z_test, c_test, M_test, force_no_2h=False)
-        # _prof._surface_mass_density_profile(R_test, RDelta_test, c_test, Delta_crit_test, rho_c_test)
-        _prof.surface_mass_density(
-            R_test, z_test, c_test, M_test, force_no_2h=False, force_no_off=False
-        )
-        _prof.excess_surface_mass_density(R_test, z_test, c_test, M_test)
-        # _prof._mean_surface_mass_density_profile(R_test, RDelta_test, c_test, Delta_crit_test, rho_c_test)
-        _prof.surface_mass_density_2h(R_test, z_test, M_test)
-        _prof.excess_surface_mass_density_2h(R_test, z_test, M_test)
-        # _prof.F_term(x_test)
-        # _prof.G_term(x_test)
-        # _prof._surface_mass_density_profile(R_test, RDelta_test, c_test, Delta_crit_test, rho_c_test)
-        # _prof._mean_surface_mass_density_profile(R_test, RDelta_test, c_test, Delta_crit_test, rho_c_test)
+    test_profiles(profile_nfw, profile_bmo)
 
     print("# clustering")
     # clustering
-    
-    H0_fid = 73.
-    background_fid = CAMBBackground(H0=H0_fid, Omega_b0=Omega_b0, Omega_cdm0=Omega_cdm0,
-                                Omega_k0=Omega_k0,
-                                As=As, ns=ns, mnu=0., w0=-1.0, wa=0.0,
-                                gamma_MG=0.0)
-    perturbations_fid = CAMBLinearPerturbations(background_fid, np.linspace(0., 2., 100))
-    
-    
-    k_min = 1e-4
-    k_max = 2e0
-    k_div = 300
-    nonu  = True
-    CL = HaloClustering(perturbations,perturbations_fid,nonu,k_div,k_min,k_max)
-    
-    z_test = np.linspace(0.,2.,20)
-    r_test = np.geomspace(20,150,30)
-    
-    CL.APcorr_func(z_test)
-    CL.WF_ra(z_test,r_test)
-    
-    
-    Pk_test = perturbations.matter_power_spectrum(
-                z_test, CL.k, hubble_units=True, k_hunit=True)
-    CL.Pk_IR_func(Pk_test)
-    
-    
-    sigma_zob = SF.scatter_zobs_z(lob_test, z_test)
-    CL.photoz_rsd_correction(z_test, sigma_zob)
+
+    H0_fid = 73.0
+    background_fid = CAMBBackground(
+        H0=H0_fid,
+        Omega_b0=Omega_b0,
+        Omega_cdm0=Omega_cdm0,
+        Omega_k0=Omega_k0,
+        As=As,
+        ns=ns,
+        mnu=0.0,
+        w0=-1.0,
+        wa=0.0,
+        gamma_MG=0.0,
+    )
+    perturbations_fid = CAMBLinearPerturbations(
+        background_fid, np.linspace(0.0, 2.0, 100)
+    )
+
+    test_clustering(background_fid, perturbations_fid)

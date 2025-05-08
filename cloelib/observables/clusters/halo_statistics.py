0a6a1b4a4ae63b7454b2a112889a9e03fb936d5e
Giorgio Lesci <giorgio.lesci2@unibo.it>
2025-05-08 11:15:58 +0200

    bug fix in halo_stat

--- cloelib/observables/clusters/halo_statistics.py
+++ cloelib/observables/clusters/halo_statistics.py
@@ -125,7 +125,7 @@ class HaloStatistics:
         rho_m_0 = (
             self.background.rho_crit(0.0)
             * self.background.Omega_m(0.0, self.nonu)
-            / self.background.h**3
+            / self.background.h**2.0
         )
         return (M / rho_m_0 * (3.0 / (4.0 * np.pi))) ** (1 / 3.0)
 
@@ -353,7 +353,7 @@ class HaloStatistics:
         """
         dlnsigmadlnR = self.dlns_dlnR(z, M)
         rho_mean_0 = self.background.Omega_m(0, self.nonu) * self.background.rho_crit(0)
-        rho_mean_0 /= self.background.h**3.0
+        rho_mean_0 /= self.background.h**2.0
 
         return rho_mean_0 / M**2.0 * self.f_sigma_nu(z, M) * dlnsigmadlnR / (-3)
 

a5c514e16b3e28c07a30a3c792a5dc8f29795c55
Giorgio Lesci <giorgio.lesci2@unibo.it>
2025-05-09 17:02:10 +0200

    reasonable tolerance for test

--- tests/test_cluster_cosmo.py
+++ tests/test_cluster_cosmo.py
@@ -37,7 +37,7 @@ def test_cosmo():
         rtol=1e-07,
     )
     assert_allclose(background.rho_crit(0), 1.27203085e11, rtol=1e-07)
-    assert_allclose(background.rdrag(), 147.50225, rtol=1e-07)
+    assert_allclose(background.rdrag(), 147.50225, rtol=1e-05)
 
     # camb linear
     perturbations = CAMBLinearPerturbations(background, np.linspace(0.0, 2.0, 100))

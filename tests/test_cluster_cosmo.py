f973519b4745a50d6df1554584c145b196cb9bae
Michel <aguena@lapp.in2p3.fr>
2025-05-09 11:13:53 +0200

    setup tests/test_cluster_cosmo.py to be able to test with all cosmos

--- tests/test_cluster_cosmo.py
+++ tests/test_cluster_cosmo.py
@@ -7,6 +7,21 @@ from cloelib.cosmology.camb_cosmology import (
     CAMBNonLinearPerturbations,
 )
 
+# from cloelib.cosmology.class_cosmology import (
+#    CLASSBackground,
+#    CLASSLinearPerturbations,
+#    CLASSNonLinearPerturbations,
+# )
+# from cloelib.cosmology.jax_cosmology import (
+#    JAXBackground,
+#    JAXLinearPerturbations,
+#    JAXNonLinearPerturbations,
+# )
+# from cloelib.cosmology.HMcode2020Emu_cosmology import (
+#    HMemuLinearPerturbations,
+#    HMemuNonLinearPerturbations,
+# )
+
 
 def test_cosmo():
     # Cosmology parameters
@@ -28,25 +43,49 @@ def test_cosmo():
         gamma_MG=0.0,
     )
 
-    # background
-    background = CAMBBackground(**_cosmo_pars)
-    assert background.Omega_m(0, nonu=True) < background.Omega_m(0, nonu=False)
-    assert_allclose(
-        background.Omega_m(0, nonu=True),
-        _cosmo_pars["Omega_cdm0"] + _cosmo_pars["Omega_b0"],
-        rtol=1e-07,
-    )
-    assert_allclose(background.rho_crit(0), 1.27203085e11, rtol=1e-07)
-    assert_allclose(background.rdrag, 147.50225, rtol=1e-07)
+    for _bkg_list, (_LinearPerturbations, _NonLinearPerturbations) in (
+        (
+            [CAMBBackground],
+            (CAMBLinearPerturbations, CAMBNonLinearPerturbations),
+        ),
+        # (
+        #    [CLASSBackground],
+        #    (CLASSLinearPerturbations, CLASSNonLinearPerturbations),
+        # ),
+        # (
+        #    [JAXBackground],
+        #    (JAXLinearPerturbations, JAXNonLinearPerturbations),
+        # ),
+        # (
+        #    [CAMBBackground, CLASSBackground, JAXBackground],
+        #    (HMemuLinearPerturbations, HMemuNonLinearPerturbations),
+        # ),
+    ):
+        for _Background in _bkg_list:
+            # background
 
-    # camb linear
-    perturbations = CAMBLinearPerturbations(background, np.linspace(0.0, 2.0, 100))
-    assert_allclose(perturbations.matter_power_spectrum(0, 1), 80.527367)
-    assert_allclose(perturbations.matter_power_spectrum(0, 1, nonu=True), 81.748209)
+            background = _Background(**_cosmo_pars)
+            assert background.Omega_m(0, nonu=True) < background.Omega_m(0, nonu=False)
+            assert_allclose(
+                background.Omega_m(0, nonu=True),
+                _cosmo_pars["Omega_cdm0"] + _cosmo_pars["Omega_b0"],
+                rtol=1e-07,
+            )
+            assert_allclose(background.rho_crit(0), 1.27203085e11, rtol=1e-07)
+            assert_allclose(background.rdrag, 147.50225, rtol=1e-07)
 
-    # camb non-linear
-    perturbations_nl = CAMBNonLinearPerturbations(
-        background, np.linspace(0.0, 2.0, 100)
-    )
-    assert_allclose(perturbations_nl.matter_power_spectrum(0, 1), 735.860906)
-    assert_allclose(perturbations_nl.matter_power_spectrum(0, 1, nonu=True), 747.017036)
+            # linear
+            perturbations = _LinearPerturbations(background, np.linspace(0.0, 2.0, 100))
+            assert_allclose(perturbations.matter_power_spectrum(0, 1), 80.527367)
+            assert_allclose(
+                perturbations.matter_power_spectrum(0, 1, nonu=True), 81.748209
+            )
+
+            # non-linear
+            perturbations_nl = _NonLinearPerturbations(
+                background, np.linspace(0.0, 2.0, 100)
+            )
+            assert_allclose(perturbations_nl.matter_power_spectrum(0, 1), 735.860906)
+            assert_allclose(
+                perturbations_nl.matter_power_spectrum(0, 1, nonu=True), 747.017036
+            )

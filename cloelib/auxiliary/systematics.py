Fixed bug in n(z) normalisation

cloelib/auxiliary/systematics.py
@@ -40,5 +40,5 @@ def shift_dndz_jax(dndz: T, z: T, dz: T) -> T:
     bins = dndz.shape[0]
     shifted = jnp.stack([interp_single_bin(i) for i in range(bins)], axis=0)
     #do the cumulative trapezoidal integral (notice the different treatment of first and last point)
-    normalization = (-0.5*(shifted[0]+shifted[-1])+jnp.sum(shifted))*(z[1]-z[0])
-    return shifted/normalization
+    normalization = (-0.5*(shifted[:,0]+shifted[:,-1])+jnp.sum(shifted, axis=1))*(z[1]-z[0])
+    return shifted / (normalization[:, None])
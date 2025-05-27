04207633bba3cfcf5d459f3557b099ac1c0fe055
Stefano Davini <davini@Stefanos-MBP.station>
2025-05-27 09:55:06 +0200

    documentation for observables init

--- cloelib/observables/__init__.py
+++ cloelib/observables/__init__.py
@@ -1,1 +1,12 @@
+"""Observable package of cloelib.
+
+The package provides photometric and spectroscopic observables.
+The Tracer protocol is used to define Photometric observables.
+The SpectroPower protocol is used to define Spectroscopic observables.
+
+Supported External Codes:
+- **SpectroPower**: `comet-emu`, `PBJ`
+"""
+
 from cloelib.observables.tracer import *
+

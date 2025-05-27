97563d9781266b2140f875a3e45980c0914c8928
Stefano Davini <davini@Stefanos-MBP.station>
2025-05-27 09:30:06 +0200

    documentation for cloelib init

--- cloelib/__init__.py
+++ cloelib/__init__.py
@@ -1,3 +1,26 @@
+"""
+cloelib – The Library for the Cosmology Likelihood for Observables in Euclid.
+
+`cloelib` is a flexible and efficient library designed to compute cosmological observables for the
+CLOE (Cosmology Likelihood for Observables in Euclid) project. It is built for seamless integration
+with Boltzmann solvers and JAX-based frameworks, enabling automatic differentiation and modularity
+for the next generation of cosmological analyses.
+
+Key Features:
+- **Intuitive & User-Friendly**: Generate Euclid-like observables (e.g., power spectra, window functions,
+  and tracer statistics) rapidly.
+- **Automatic Differentiation**: Includes a toy example with `JAX` for gradient-based computations.
+- **Modular & Extensible**:
+    - Easily interface with external Boltzmann solvers or emulators via Python `Protocols` following
+      the cosmology.API.
+    - Core structure enables defining Background & Perturbation models, choosing observables via
+      Tracer or SpectroPower protocols, and computing final summary statistics like angular power spectra
+      or Legendre multipoles.
+
+License:
+This project is licensed under the MIT LICENSE.
+"""
+
 __all__ = []
 
 from cloelib.cosmology import *

1bf6f74085efef39c5e18ff5e8743d68452d6115
marcobonici <bonici.marco@gmail.com>
2025-06-02 22:36:56 -0400

    Update test_math_utils.py

--- cloelib/tests/test_math_utils.py
+++ cloelib/tests/test_math_utils.py
@@ -4,5 +4,5 @@ from scipy import integrate
 from numpy.testing import assert_array_equal
 
 def test_simpson_even():
-    assert_array_equal(simpsons_weights_jit(11) == simpsons_weights_odd(11))
-    assert_array_equal(simpsons_weights_jit(10) == simpsons_weights_even(10))
+    assert_array_equal(simpsons_weights_jit(11), simpsons_weights_odd(11))
+    assert_array_equal(simpsons_weights_jit(10), simpsons_weights_even(10))

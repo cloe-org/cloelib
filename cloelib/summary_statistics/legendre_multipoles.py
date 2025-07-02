gh-227: Fix problem with missing factor of 2 in angular integration of individual terms of loop expansion (#228)
Co-authored-by: Andrea Pezzotta <pezzotta@Andreas-MacBook-Pro.local>

cloelib/summary_statistics/legendre_multipoles.py
@@ -306,7 +306,7 @@ class LegendreMultipoles:
             leg = legendre(ell, self.mu_grid)
             multipoles[f'ell{ell}'] = \
                 np.einsum("abc,c,c->ab", Pk2d, leg, self.mu_weights)
-            multipoles[f'ell{ell}'] *= prefactors[i]
+            multipoles[f'ell{ell}'] *= (2.0 * prefactors[i])
         return multipoles
 
     def convolved_power_multipoles(self, mixing_matrix: dict,
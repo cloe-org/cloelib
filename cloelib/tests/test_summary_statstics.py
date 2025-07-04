e7d851dc4a2cd7baffb8139905a203c382ac6d55
marcobonici <bonici.marco@gmail.com>
2025-07-04 01:56:40 -0400

    Update test_summary_statstics.py

--- cloelib/tests/test_summary_statstics.py
+++ cloelib/tests/test_summary_statstics.py
@@ -1,5 +1,5 @@
 import pytest
-import cloelib.summary_statistics.angular_correlation_function_wigner
+from  cloelib.summary_statistics import angular_correlation_function_wigner
 
 import jax.numpy as jnp
 from jax import random
@@ -9,7 +9,7 @@ def test_wigner_cache():
     beta = 1.
     ell = jnp.linspace(2,200)
     d_2_0_ell()
-    assert d_0_0_ell(beta, ell) == _d_0_0_ell_compute(beta, ell)
-    assert d_2_0_ell(beta, ell) == _d_2_0_ell_compute(beta, ell)
-    assert d_2_2_ell(beta, ell) == _d_2_2_ell_compute(beta, ell)
-    assert d_2_m2_ell(beta, ell) == _d_2_m2_ell_compute(beta, ell)
+    assert angular_correlation_function_wigner.d_0_0_ell(beta, ell) == angular_correlation_function_wigner._d_0_0_ell_compute(beta, ell)
+    assert angular_correlation_function_wigner.d_2_0_ell(beta, ell) == angular_correlation_function_wigner._d_2_0_ell_compute(beta, ell)
+    assert angular_correlation_function_wigner.d_2_2_ell(beta, ell) == angular_correlation_function_wigner._d_2_2_ell_compute(beta, ell)
+    assert angular_correlation_function_wigner.d_2_m2_ell(beta, ell) == angular_correlation_function_wigner._d_2_m2_ell_compute(beta, ell)

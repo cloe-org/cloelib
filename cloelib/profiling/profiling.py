1c4f41b0928079ea968c0ea7aa71e9c321f52168
Filippo Oppizzi <50492103+ippoppi@users.noreply.github.com>
2025-07-15 12:46:09 +0200

    Update profiling.py

--- cloelib/profiling/profiling.py
+++ cloelib/profiling/profiling.py
@@ -15,17 +15,17 @@ PROFILER_INTERVAL = 0.001
 
 
 class _profiling_active:
-"""class to avoid multiple call of the profiling in case of decorated nested fuctions"""
+    """class to avoid multiple call of the profiling in case of decorated nested fuctions"""
     is_active=False
     
  
 def _is_profiling_enabled():
-"""check the profiling is setted enabled"""
+    """check the profiling is setted enabled"""
     return os.environ.get('ENABLE_PROFILING') == 'true'
 
 
 def _get_output():
-""" get the output directory"""
+    """ get the output directory"""
     env_val = os.environ.get('PROFILE_OUTPUT_DIR')
     if env_val: 
         return env_val
@@ -33,7 +33,7 @@ def _get_output():
 
 
 def _get_pyinstrument_interval():
-""" get the sampling interval of pyinstrument (larger interval=lower overhead) """
+    """ get the sampling interval of pyinstrument (larger interval=lower overhead) """
     env_val = os.environ.get('PROFILING_INTERVAL')
     if env_val: 
         return float(env_val)
@@ -41,26 +41,26 @@ def _get_pyinstrument_interval():
 
 
 def enable_profiling():
-""" enable the time profiling """
+    """ enable the time profiling """
     os.environ['ENABLE_PROFILING'] = 'true'
     print("Profiling: Enabled.")
 
 def disable_profiling():
-"""Disable the time profiling""" 
+    """Disable the time profiling""" 
     if 'ENABLE_PROFILING' in os.environ:
         del os.environ['ENABLE_PROFILING']
     print("Profiling: Disabled.")
 
 def set_output(outdir):
-"""Set the output directory from the local path"""
+    """Set the output directory from the local path"""
     os.environ['PROFILE_OUTPUT_DIR'] = str(outdir)
 
 def set_interval(interval):
-"""Set the pyinstrument sampling interval"""
+    """Set the pyinstrument sampling interval"""
     os.environ['PROFILING_INTERVAL'] = str(interval)
 
 def profile_function(func):
-"""Decorator to profile a function's runtime using pyinstrument"""
+    """Decorator to profile a function's runtime using pyinstrument"""
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if _is_profiling_enabled() and not _profiling_active.is_active:

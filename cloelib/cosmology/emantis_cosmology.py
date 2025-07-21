f87c815f6b58a0c8ef4869527ab9721c771deeeb
Iñigo Sáez Casares <inigo.saez@unimi.it>
2025-07-21 10:38:42 +0200

    Implementation of emantis emulator + unit tests.

---
+++ cloelib/cosmology/emantis_cosmology.py
@@ -0,0 +1,169 @@
+import numpy as np
+from cloelib.auxiliary.extrapolator import extend_spectra
+from cloelib.cosmology.cosmology import Background, Perturbations
+from emantis.matter_power_spectrum import NonLinearMGBoostEmulator
+from scipy import interpolate
+
+
+class EmantisFofrNonLinearPerturbations:
+    """Nonlinear perturbations in f(R) gravity.
+
+    The nonlinear matter power spectrum boost, i.e. P_f(R)(k) / P_LCDM(k), is computed with the e-MANTIS emulator.
+    The boost is then combined with an external nonlinear LCDM matter power spectrum prediction
+    in order to obtain the full nonlinear matter power spectrum in f(R) gravity.
+    """
+
+    def __init__(
+        self,
+        background: Background,
+        nonlinearperturbations_lcdm: Perturbations,
+        fR0: float,
+        redshifts: np.ndarray,
+        verbose: bool = False,
+        emu_version: int = 2,
+    ) -> None:
+        """Initialize the perturbation instance.
+
+        Parameters
+        ----------
+        background : Background
+            Background cosmology object, providing all the standard cosmological parameters.
+        nonlinearperturbations_lcdm : Perturbations
+            A nonlinear perturbations object, providing the nonlinear matter power spectrum in LCDM.
+        fR0 : float
+            The modified gravity parameter fR0.
+        redshifts : np.ndarray
+            An array of redshifts used to compute the matter power spectrum.
+        verbose : float, optional (default=False)
+            Activate or not verbose output from the emantis emulator.
+        emu_version : int, optional (default=2)
+            The version of the emantis emulator to use.
+            Version 2 (the default) has an extended cosmological parameter and redshift range.
+        """
+        self.background = background
+        self.nonlinearpertubations_lcdm = nonlinearperturbations_lcdm
+
+        # Check and process emulator version.
+        if emu_version == 2:
+            model = "fR"
+        elif emu_version == 1:
+            model = "fR_v1"
+        else:
+            raise ValueError(
+                f"Unsupported value for `emu_version`. Allowed values are 1 or 2."
+            )
+
+        # Initialize the e-MANTIS emulator.
+        self.emantis_emu = NonLinearMGBoostEmulator(model=model, verbose=verbose)
+
+        # Build parameter dict. for the emulator.
+        # These are the parameters common to the v1 and v2 of the emulator.
+        self.params_emu = {
+            "Omega_m": self.background.Omega_cdm0
+            + self.background.Omega_b0
+            + self.background.mnu / 93.14 / self.background.h**2,
+            "A_s": self.background.As,
+            "logfR0": -np.log10(np.abs(fR0)),
+        }
+
+        # Add extra parameters supported only by the v2 of the emulator.
+        if emu_version == 2:
+            self.params_emu["n_s"] = self.background.ns
+            self.params_emu["h"] = self.background.h
+            self.params_emu["Omega_b"] = self.background.Omega_b0
+
+        # Compute scale factor values.
+        aexp_values = 1 / (1 + redshifts)
+
+        # Minimum scale factor and maximum redshift of the emulator.
+        aexp_min = np.min(self.emantis_emu.aexp_nodes)
+        z_max = 1 / aexp_min - 1
+
+        # Select scale factor and redshift values within emulator range.
+        z_emu = redshifts[redshifts <= z_max]
+        aexp_emu = aexp_values[aexp_values >= aexp_min]
+
+        # Get default wavenumber bins from the emulator (h/Mpc -> 1/Mpc).
+        k_emu = self.emantis_emu.kbins * self.params_emu["h"]
+
+        # Let the emulator do it's own extrapolation to low-k values.
+        k_min = 0.0001
+        k_emu = np.concatenate(
+            [np.linspace(k_min, k_emu[0], 100, endpoint=False), k_emu]
+        )
+
+        # Get boost from emantis (pass wavenumbers in h/Mpc).
+        pk_boost_emu = self.emantis_emu.predict_boost(
+            self.params_emu, aexp_emu, k=k_emu / self.params_emu["h"]
+        )
+
+        # Extrapolate emulator prediction in wavenumber and redshift.
+        k_extended, z_extended, pk_boost_extended = extend_spectra(
+            k_emu,
+            z_emu,
+            pk_boost_emu,
+            flag_range=True,
+            option_wavenumber="const",
+            option_redshift="power_law",
+            option_cosmo="const",
+            extrap_z=redshifts,
+            ns=self.background.ns,
+        )
+
+        self.k = k_extended
+        self.z = z_extended
+
+        # Get LCDM matter power spectrum.
+        pk_lcdm = self.nonlinearpertubations_lcdm.matter_power_spectrum(self.z, self.k)
+
+        # Build interpolation for full power spectrum.
+        self.pk_interp = interpolate.RectBivariateSpline(
+            self.z, self.k, pk_lcdm * pk_boost_extended
+        )
+
+    def matter_power_spectrum(self, zs, ks) -> np.ndarray:
+        r"""Compute the nonlinear matter power spectrum.
+
+        Parameters
+        ----------
+        zs: numpy.ndarray
+            Redshift values at which to compute the matter power spectrum.
+
+        ks: numpy.ndarray
+            Wavenumber values at which to compute the matter power spectrum, in units of h/Mpc.
+
+        Returns
+        -------
+        pk: numpy.ndarray
+            Nonlinear matter power spectrum at the input redshift and wavenumber values.
+        """
+
+        return self.pk_interp(zs, ks)
+
+    def growth_factor(self, zs, ks) -> np.ndarray:
+        r"""
+        Compute the growth factor for some input redshift and wavenumber values.
+
+        .. math::
+            D(z, k) =\sqrt{P_{\rm \delta\delta}(z, k)\
+            /P_{\rm \delta\delta}(z=0, k)}\\
+
+        and normalizes as for :math:`D(z)/D(0)`.
+
+        Parameters:
+        -----------
+        zs : array_like
+            Redshift values at which to compute the growth factor.
+        ks : array_like
+            Wavenumber values at which to compute the growth factor.
+
+        Returns:
+        --------
+        np.ndarray
+            The growth factor for the input redshift and wavenumber values.
+        """
+        D_z_k = np.sqrt(
+            self.matter_power_spectrum(zs, ks) / self.matter_power_spectrum(0, ks)
+        )
+
+        return D_z_k
---
+++ cloelib/tests/test_emantis.py
@@ -0,0 +1,89 @@
+import numpy as np
+from cloelib.cosmology.emantis_cosmology import EmantisFofrNonLinearPerturbations
+from emantis.matter_power_spectrum import NonLinearMGBoostEmulator
+
+
+class TestBackground:
+
+    def __init__(self) -> None:
+
+        self.h = 0.7
+        self.Omega_b0 = 0.05
+        self.Omega_cdm0 = 0.27
+        self.mnu = 0
+        self.As = 2e-9
+        self.ns = 0.96
+
+
+class LCDMNonLinearPerturbations:
+
+    def __init__(self) -> None:
+        pass
+
+    def matter_power_spectrum(self, zs, ks):
+        return np.ones((zs.shape[0], ks.shape[0]))
+
+
+def test_mg_boost_cloelib_vs_external():
+    """Validate the nonlinear matter power spectrum boost.
+
+    The boost obtained with the cloelib interface of emantis is compared to the boost
+    obtained directly from emantis.
+    """
+
+    # Redshift and wavenumber arrays.
+    zs = np.linspace(0, 3, 100)
+    ks = np.geomspace(1e-4, 5, 100)
+
+    fR0 = -1e-5
+
+    # Init. background.
+    background = TestBackground()
+
+    # Init. LCDM nonlinear perturbations.
+    lcdm_nonlinear = LCDMNonLinearPerturbations()
+
+    # Init. f(R) nonlinear perturbations.
+    emantis_cloe = EmantisFofrNonLinearPerturbations(
+        background=background,
+        nonlinearperturbations_lcdm=lcdm_nonlinear,
+        redshifts=zs,
+        fR0=fR0,
+    )
+
+    # Init. e-MANTIS emulator from emantis directly.
+    emantis_ext = NonLinearMGBoostEmulator(model="fR")
+
+    emantis_ext_params = {
+        "Omega_m": background.Omega_cdm0
+        + background.Omega_b0
+        + background.mnu / 93.14 / background.h**2,
+        "Omega_b": background.Omega_b0,
+        "A_s": background.As,
+        "n_s": background.ns,
+        "h": background.h,
+        "logfR0": -np.log10(np.abs(fR0)),
+    }
+
+    # Compute e-MANTIS boost by calling emantis directly (k in units of h/Mpc).
+    boost_ext = emantis_ext.predict_boost(
+        emantis_ext_params, aexp=1 / (1 + zs), k=ks / background.h
+    )
+
+    # Compute f(R) power spectrum.
+    pk_fr = emantis_cloe.matter_power_spectrum(zs, ks)
+
+    # Compute LCDM power spectrum.
+    pk_lcdm = lcdm_nonlinear.matter_power_spectrum(zs, ks)
+
+    # Compute power spectrum boost.
+    boost_cloe = pk_fr / pk_lcdm
+
+    # Compute absolute relative difference.
+    abs_rel_diff = np.abs(boost_cloe - boost_ext) / boost_ext
+
+    # Maximum absolute relative difference is smaller than 0.2%.
+    assert np.max(abs_rel_diff) < 2e-3
+
+    # Mean absolute relative difference is smaller than 0.02%.
+    assert np.mean(abs_rel_diff) < 2e-4

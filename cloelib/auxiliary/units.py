2492fcad35295cf37291efc100ef483bbde67b02
Michel <aguena@lapp.in2p3.fr>
2025-08-01 12:39:56 +0200

    rm astropy

--- cloelib/auxiliary/units.py
+++ cloelib/auxiliary/units.py
@@ -1,7 +1,5 @@
 """The constants used across the code are defined in this module."""
-from astropy import units
-from astropy.constants import G
 
 SPEED_OF_LIGHT = 2.99792458E8         # Speed of light in m/s
-GRAVITATIONAL_CONSTANT = G.to(units.Mpc**3.0 / (units.Msun * units.s**2.0)).value # Gravitational constant in Mpc^3/Msun/s^2
-MPC_TO_KM = units.Mpc.to(units.km) # 1 Mpc in km
+GRAVITATIONAL_CONSTANT = 4.51710305E-48 # Gravitational constant in Mpc^3/Msun/s^2
+MPC_TO_KM = 3.08567758E+19 # 1 Mpc in km

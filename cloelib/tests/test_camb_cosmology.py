166972030506d0f8bff72508dce495fd34e2d680
Arthur Loureiro <arthur.loureiro@fysik.su.se>
2025-08-12 15:06:51 +0200

    propagated changes in the protocol to tests

--- cloelib/tests/test_camb_cosmology.py
+++ cloelib/tests/test_camb_cosmology.py
@@ -16,7 +16,7 @@ def camb_background_instance(scope="module"):
     camb_instance = CAMBBackground(H0=H0, Omega_b0=Omega_b0, Omega_cdm0=Omega_cdm0, 
                                    Omega_k0=0.,
                                    As=2e-9, ns=0.96, mnu=0., w0=-1.0, wa=0.0, 
-                                   gamma_MG=0.0)
+                                   gamma_MG=0.0, N_mnu=0)
     return camb_instance
 
 def test_camb_background_required_methods():

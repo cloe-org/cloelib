42f79ed9eea53362e72e180cf4ac72ecdd43959f
Arthur Loureiro <arthur.loureiro@fysik.su.se>
2025-08-14 11:40:05 +0200

    trying to fix mypy error in the CI

--- cloelib/cosmology/jax_cosmology.py
+++ cloelib/cosmology/jax_cosmology.py
@@ -118,7 +118,7 @@ class JAXBackground:
                 # added for consistency
                 raise ValueError(f"Expected {N_mnu} individual neutrino masses, "
                                      f"but got {len(mnu)}: {mnu}")
-            return np.sum(mnu)
+            return float(np.sum(mnu))
         else:
             return mnu
 

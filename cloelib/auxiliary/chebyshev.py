a49e5c69fe7c46bbe21f11d5984545039c2a2cfb
Matteo Baratto <teo.baratto.97@gmail.com>
2025-10-21 17:33:24 +0200

    Added clenshaws curtis quadrature

--- cloelib/auxiliary/chebyshev.py
+++ cloelib/auxiliary/chebyshev.py
@@ -64,3 +64,28 @@ def chebyshev_interpolation(x: Array, c: Array) -> Array:
     x_scaled = (2 * x - (x.min() + x.max())) / (x.max() - x.min())
     return np.polynomial.chebyshev.chebval(x_scaled, c)
 
+def clenshaws_curtis_quadrature(n: int, a: float, b: float) -> tuple[Array, Array]:
+    """Compute the Clenshaw-Curtis quadrature of a function f on [a, b].
+
+    Args:
+        n: Number of Chebyshev points to use.
+        a: Start of the interval.
+        b: End of the interval.
+    Returns:
+        cheb_pts : Chebyshev points on [a, b].
+        weights : Corresponding weights for Clenshaw-Curtis quadrature.
+    """
+    
+    # Modified Chebyshev moments of the first kind
+    mu = jnp.zeros(n, dtype=float)
+    for i in range(0, n, 2):
+        if i != 1:
+            mu[i] = 2.0 / (1.0 - i**2)
+
+    # Compute weights using the modified Chebyshev moments
+    w = jax.scipy.fft.dct(mu, type=2, norm=None) / n
+    w = w.at[0].multiply(0.5)
+    #c = c.at[N].multiply(0.5)
+    w = (b - a) / 2 * w
+
+    return chebyshev_points_interval(n, a, b), w

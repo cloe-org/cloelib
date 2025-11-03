1ebe43d4898f3b69be8475f5a3fc3a4250a6d979
Marco Bonici <58727599+marcobonici@users.noreply.github.com>
2025-11-03 07:28:38 -0500

    Update cloelib/cosmology/camb_cosmology.py

--- cloelib/cosmology/camb_cosmology.py
+++ cloelib/cosmology/camb_cosmology.py
@@ -428,7 +428,7 @@ class CAMBNonLinearPerturbations:
         self.kmax = 500
         if 0.001 < abs(redshifts.min()):
             warnings.warn(
-                "lowest redshift should be close to 0.0 for proper interpolation to small redshifts and the computation of sigma8(z=0)."
+                "Lowest redshift should be close to 0.0 for proper interpolation to small redshifts and the computation of sigma8(z=0)."
             )
         self.z = redshifts
 

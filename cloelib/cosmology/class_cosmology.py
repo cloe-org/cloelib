02e29fd56f54361f3f78787fed646c58e813f722
zizgitlab <ziad.sakr@net.usj.edu.lb>
2026-01-16 14:37:20 +0100

    completion of adding sigma8 to CLASS

--- cloelib/cosmology/class_cosmology.py
+++ cloelib/cosmology/class_cosmology.py
@@ -379,7 +379,7 @@ class CLASSLinearPerturbations:
             The sigma8 value.
         """
 
-        return 0
+        return self.results.sigma8()
 
 
 class CLASSNonLinearPerturbations:
@@ -499,4 +499,4 @@ class CLASSNonLinearPerturbations:
             The sigma8 value.
         """
 
-        return 0
+        return self.results.sigma8()

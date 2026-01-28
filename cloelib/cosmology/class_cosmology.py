10f6e4c5b3183d088343060d4f192bb1658eeafe
zizgitlab <ziad.sakr@net.usj.edu.lb>
2026-01-28 14:55:47 +0100

    fix CLASS compatibility with photo.py or other modules that need Om_0, Ob_0 or k

--- cloelib/cosmology/class_cosmology.py
+++ cloelib/cosmology/class_cosmology.py
@@ -248,7 +248,11 @@ class CLASSBackground:
         Returns:
             (np.ndarray): Matter density values.
         """
-        return np.array([self.results.Om_m(z) for z in zs])
+        try:
+            Omegam_0 = np.array([self.results.Om_m(z) for z in zs])
+        except TypeError:
+            Omegam_0 = self.results.Om_m(zs)
+        return Omegam_0
 
     def Omega_b(self, zs: np.ndarray) -> np.ndarray:
         """
@@ -260,7 +264,11 @@ class CLASSBackground:
         Returns:
             (np.ndarray): Matter density values.
         """
-        return np.array([self.results.Om_b(z) for z in zs])
+        try:
+            Omegab_0 = np.array([self.results.Om_b(z) for z in zs])
+        except TypeError:
+            Omegab_0 = self.results.Om_b(zs)
+        return Omegab_0
 
     @property
     def rdrag(self) -> float:
@@ -290,6 +298,7 @@ class CLASSLinearPerturbations:
         self.results = Class()
         self.results.set(self.interface_args["CLASSparams"])
         self.results.compute()
+        self.k = np.logspace(np.log10(1e-4), np.log10(self.kmax), 100)
 
     @property
     def _interface_args(self) -> dict:
@@ -396,6 +405,7 @@ class CLASSNonLinearPerturbations:
         self.results = Class()
         self.results.set(self.interface_args["CLASSparams"])
         self.results.compute()
+        self.k = np.logspace(np.log10(1e-4), np.log10(self.kmax), 100)
 
     def matter_power_spectrum(
         self, zs, ks, hubble_units=False, k_hunit=False

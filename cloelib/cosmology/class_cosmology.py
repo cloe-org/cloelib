f30781019feb8cdeafa52e30df791a04dd7b166e
zizgitlab <ziad.sakr@net.usj.edu.lb>
2026-02-12 15:49:37 +0100

    remove call by iteration of Om and Ob

--- cloelib/cosmology/class_cosmology.py
+++ cloelib/cosmology/class_cosmology.py
@@ -260,7 +260,7 @@ class CLASSBackground:
         Returns:
             (np.ndarray): Matter density values.
         """
-        return np.array([self.results.Om_m(z) for z in zs])
+        return self.results.Om_m(zs)
 
     def Omega_b(self, zs: np.ndarray) -> np.ndarray:
         """
@@ -272,7 +272,7 @@ class CLASSBackground:
         Returns:
             (np.ndarray): Matter density values.
         """
-        return np.array([self.results.Om_b(z) for z in zs])
+        return self.results.Om_b(zs)
 
     @property
     def rdrag(self) -> float:

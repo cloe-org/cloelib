c11d49119e0db4c823beeed4eefac80ee1048d52
garico92 <arico.giovanni.92@gmail.com>
2026-03-20 18:01:55 +0100

    added baccoemu methods for matter_power_spectrum_cb and growth_rate to comply with Perturbations. Externalised emulator initialisation

--- cloelib/cosmology/baccoemu_cosmology.py
+++ cloelib/cosmology/baccoemu_cosmology.py
@@ -16,24 +16,33 @@ try:
 except ImportError:
     raise ImportError("BACCOemu could not be imported")
 
+emu = {}
+emu["linear"] = baccoemu.Matter_powerspectrum(
+    verbose=False, nonlinear_boost=False, baryonic_boost=False
+)
+for nonlinear_model in ["Angulo2021", "Arico2023"]:
+    emu[nonlinear_model] = {}
+    for baryonic_model in ["Arico2021", "Burger2025"]:
+        emu[nonlinear_model][baryonic_model] = baccoemu.Matter_powerspectrum(
+            verbose=False,
+            nonlinear_model_name=nonlinear_model,
+            baryonic_boost=True,
+            baryonic_model_name=baryonic_model,
+        )
+
 
 class BACCOemuLinearPerturbations:
     """Class for perturbations cosmology using BACCOemu, compatibly with the Perturbations protocol."""
 
-    def __init__(
-        self, background: Background, redshifts: np.ndarray, cold: bool = False
-    ):
+    def __init__(self, background: Background, redshifts: np.ndarray):
         """Intialize the BACCOemuLinearPerturbations instance."""
         assert background.Omega_k0 == 0, "Non flat geometries not supported"
 
-        self.emu = baccoemu.Matter_powerspectrum(
-            verbose=False, nonlinear_boost=False, baryonic_boost=False
-        )
+        self.emu = emu["linear"]
         redshift_max = 1 / self.emu.emulator["linear"]["bounds"][-1][0].item() - 1
 
         self.z = redshifts[redshifts <= redshift_max]
         self.background = background
-        self.cold = cold
 
         self.params_emu = {
             "omega_cold": self.background.Omega_cdm0 + self.background.Omega_b0,
@@ -48,7 +57,7 @@ class BACCOemuLinearPerturbations:
 
         self.params_emu["expfactor"] = 1 / (1 + self.z)
 
-        k_emu, Pk = self.emu.get_linear_pk(cold=self.cold, **self.params_emu)
+        k_emu, Pk = self.emu.get_linear_pk(cold=False, **self.params_emu)
 
         # Warning: a lot of parameters currently hard-coded
         k_out, z_out, Pk_out = extend_spectra(
@@ -63,16 +72,36 @@ class BACCOemuLinearPerturbations:
             ns=self.background.ns,
         )
 
+        k_emu, Pk_cb = self.emu.get_linear_pk(cold=True, **self.params_emu)
+
+        # Warning: a lot of parameters currently hard-coded
+        k_out, z_out, Pk_cb_out = extend_spectra(
+            k_emu * self.background.h,
+            self.z,
+            Pk_cb * self.background.h**-3,
+            flag_range=True,
+            option_wavenumber="logk2",
+            option_redshift="power_law",
+            extrap_z=redshifts,
+            option_cosmo="const",
+            ns=self.background.ns,
+        )
+
         self.k = k_out
         self.z = z_out
         self.Pk = Pk_out
+        self.Pk_cb = Pk_cb_out
 
         pk_interp = interpolate.RectBivariateSpline(self.z, self.k, Pk_out, kx=1, ky=1)
+        pk_cb_interp = interpolate.RectBivariateSpline(
+            self.z, self.k, Pk_cb_out, kx=1, ky=1
+        )
 
         self.Pk_interp = pk_interp
+        self.Pk_cb_interp = pk_cb_interp
 
     def matter_power_spectrum(self, zs, ks) -> np.ndarray:
-        r"""Compute the linear matter power spectrum.
+        r"""Compute the total (dark matter + baryons + neutrinos) linear matter power spectrum.
 
         Args:
             ks (numpy.ndarray): Wave number in h Mpc^{-1}
@@ -84,9 +113,22 @@ class BACCOemuLinearPerturbations:
         """
         return self.Pk_interp(zs, ks)
 
+    def matter_power_spectrum_cb(self, zs, ks) -> np.ndarray:
+        r"""Compute the cold (dark matter + baryons) linear matter power spectrum.
+
+        Args:
+            ks (numpy.ndarray): Wave number in h Mpc^{-1}
+            zs (numpy.ndarray): redshifts
+
+        Returns:
+            pk (numpy.ndarray): Linear matter power spectrum at the specified scale and redshift
+
+        """
+        return self.Pk_cb_interp(zs, ks)
+
     def growth_factor(self, zs, ks) -> np.ndarray:
         r"""
-        Calculate the growth factor for given redshifts and wavenumbers.
+        Calculate the total (dark matter + baryons + neutrinos) growth factor for given redshifts and wavenumbers.
 
         $$
             D(z, k) =\sqrt{P_{\rm \delta\delta}(z, k)\
@@ -107,27 +149,123 @@ class BACCOemuLinearPerturbations:
 
         return D_z_k
 
+    def growth_rate(self, zs, ks) -> np.ndarray:
+        r"""
+        Calculate the total (dark matter + baryons + neutrinos) linear growth rate for given redshifts and wavenumbers.
+
+        Args:
+            zs (array_like): Redshifts at which to calculate the growth factor.
+            ks (array_like): Wavenumbers at which to calculate the growth factor.
+
+        Returns:
+            (np.ndarray): The linear growth rate as a function of redshift and wavenumber.
+        """
+
+        growth = self.growth_factor(zs, ks)
+        lna = np.log(1 / (1 + zs))
+
+        # sort by increasing lna
+        idx = np.argsort(lna)
+        lna_sorted = lna[idx]
+        growth_sorted = growth[idx, :]
+
+        cs = interpolate.CubicSpline(lna_sorted, np.log(growth_sorted), axis=0)
+        f = cs(lna_sorted, 1)  # d ln D / d ln a, shape (nz, nk)
+
+        unsort = np.argsort(idx)
+        return f[unsort, :]
+
+    def growth_factor_cb(self, zs, ks) -> np.ndarray:
+        r"""
+        Calculate the cold (dark matter + baryons) growth factor for given redshifts and wavenumbers.
+
+        $$
+            D(z, k) =\sqrt{P_{\rm \delta\delta}(z, k)\
+            /P_{\rm \delta\delta}(z=0, k)}
+        $$
+
+        and normalizes as for $D(z)/D(0)$.
+
+        Args:
+            zs (array_like): Redshifts at which to calculate the growth factor.
+            ks (array_like): Wavenumbers at which to calculate the growth factor.
+
+        Returns:
+            (np.ndarray): The growth factor as a function of redshift and wavenumber.
+        """
+        if hasattr(self, "Pk_cb_interp") and self.Pk_cb_interp is not None:
+            D_z_k = np.sqrt(self.Pk_cb_interp(zs, ks) / self.Pk_cb_interp(0, ks))
+
+        return D_z_k
+
+    def growth_rate_cb(self, zs, ks) -> np.ndarray:
+        r"""
+        Calculate the cold (dark matter + baryons) linear growth rate for given redshifts and wavenumbers.
+
+        Args:
+            zs (array_like): Redshifts at which to calculate the growth factor.
+            ks (array_like): Wavenumbers at which to calculate the growth factor.
+
+        Returns:
+            (np.ndarray): The linear cold growth rate as a function of redshift and wavenumber.
+        """
+        growth = self.growth_factor_cb(zs, ks)
+        lna = np.log(1 / (1 + zs))
+
+        # sort by increasing lna
+        idx = np.argsort(lna)
+        lna_sorted = lna[idx]
+        growth_sorted = growth[idx, :]
+
+        cs = interpolate.CubicSpline(lna_sorted, np.log(growth_sorted), axis=0)
+        f = cs(lna_sorted, 1)  # d ln D / d ln a, shape (nz, nk)
+
+        unsort = np.argsort(idx)
+        return f[unsort, :]
+
     def sigma8_0(self) -> float:
         """
-        Calculate the sigma8 value for the current cosmology.
+        Calculate the total (dark matter + baryons + neutrinos) linear sigma8 value for the current cosmology.
 
         Returns:
         --------
         float
             The sigma8 value.
         """
-        return self.emu.get_sigma8(cold=self.cold, **self.params_emu)
+        return np.asarray(self.emu.get_sigma8(cold=False, **self.params_emu))[0]
 
     def sigma12_0(self) -> float:
         """
-        Calculate the sigma12 value for the current cosmology.
+        Calculate the total (dark matter + baryons + neutrinos) linear  sigma12 value for the current cosmology.
 
         Returns:
         --------
         float
             The sigma8 value.
         """
-        return self.emu.get_sigma12(cold=self.cold, **self.params_emu)
+        return np.asarray(self.emu.get_sigma12(cold=False, **self.params_emu))[0]
+
+    def sigma8_0_cb(self) -> float:
+        """
+        Calculate the  cold (dark matter + baryons) linear  sigma8 value for the current cosmology.
+
+        Returns:
+        --------
+        float
+            The sigma8 value.
+        """
+        return np.asarray(self.emu.get_sigma8(cold=True, **self.params_emu))[0]
+
+    def sigma12_0_cb(self) -> float:
+        """
+        Calculate the  cold (dark matter + baryons) linear sigma12 value for the current cosmology.
+
+        Returns:
+        --------
+        float
+            The sigma8 value.
+        """
+        return np.asarray(self.emu.get_sigma12(cold=True, **self.params_emu))[0]
 
 
 class BACCOemuNonLinearPerturbations:
@@ -138,7 +276,6 @@ class BACCOemuNonLinearPerturbations:
         background: Background,
         linearperturbations: Perturbations,
         redshifts: np.ndarray,
-        cold: Optional[bool] = False,
         nonlinear_model_name: Optional[str] = "Arico2023",
         baryonic_boost: Optional[str] = None,
         baryonic_model_name: Optional[str] = "Burger2025",
@@ -153,17 +290,12 @@ class BACCOemuNonLinearPerturbations:
         """Initialize the BACCOemuNonLinearPerturbations instance."""
         assert background.Omega_k0 == 0, "Non flat geometries not supported"
 
-        self.emu = baccoemu.Matter_powerspectrum(
-            verbose=False,
-            nonlinear_model_name=nonlinear_model_name,
-            baryonic_boost=baryonic_boost,
-            baryonic_model_name=baryonic_model_name,
-        )
-
+        self.emu = emu[nonlinear_model_name][baryonic_model_name]
         redshift_max = 1 / self.emu.emulator["nonlinear"]["bounds"][-1][0].item() - 1
 
         self.z = redshifts[redshifts <= redshift_max]
         self.background = background
+        self.linearperturbations = linearperturbations
 
         self.params_emu = {
             "omega_cold": self.background.Omega_cdm0 + self.background.Omega_b0,
@@ -199,7 +331,11 @@ class BACCOemuNonLinearPerturbations:
         )
 
         _, Pk = self.emu.get_nonlinear_pk(
-            cold=cold, baryonic_boost=baryonic_boost, k=all_k, **self.params_emu
+            cold=False, baryonic_boost=baryonic_boost, k=all_k, **self.params_emu
+        )
+
+        _, Pk_cb = self.emu.get_nonlinear_pk(
+            cold=True, baryonic_boost=baryonic_boost, k=all_k, **self.params_emu
         )
 
         # Warning: a lot of parameters currently hard-coded
@@ -207,6 +343,18 @@ class BACCOemuNonLinearPerturbations:
             all_k * self.background.h,
             self.z,
             Pk * self.background.h**-3,
+            flag_range=True,
+            option_wavenumber="power_law",
+            option_redshift="power_law",
+            extrap_z=redshifts,
+            option_cosmo="const",
+            ns=self.background.ns,
+        )
+
+        k_out, z_out, Pk_cb_out = extend_spectra(
+            all_k * self.background.h,
+            self.z,
+            Pk_cb * self.background.h**-3,
             flag_range=True,
             option_wavenumber="power_law",
             option_redshift="power_law",
@@ -228,13 +376,18 @@ class BACCOemuNonLinearPerturbations:
         self.k = k_out
         self.z = z_out
         self.Pk = Pk_out
+        self.Pk_cb = Pk_cb_out
 
         pk_interp = interpolate.RectBivariateSpline(self.z, self.k, self.Pk, kx=1, ky=1)
+        pk_cb_interp = interpolate.RectBivariateSpline(
+            self.z, self.k, self.Pk_cb, kx=1, ky=1
+        )
 
         self.Pk_interp = pk_interp
+        self.Pk_cb_interp = pk_cb_interp
 
     def matter_power_spectrum(self, zs, ks) -> np.ndarray:
-        r"""Compute the linear matter power spectrum.
+        r"""Compute the total (dark matter + baryons + neutrinos) linear matter power spectrum.
 
         Args:
             ks (numpy.ndarray): Wave number in h Mpc^{-1}
@@ -246,13 +399,26 @@ class BACCOemuNonLinearPerturbations:
         """
         return self.Pk_interp(zs, ks)
 
+    def matter_power_spectrum_cb(self, zs, ks) -> np.ndarray:
+        r"""Compute the cold (dark matter + baryons) linear matter power spectrum.
+
+        Args:
+            ks (numpy.ndarray): Wave number in h Mpc^{-1}
+            zs (numpy.ndarray): redshifts
+
+        Returns:
+            pk (numpy.ndarray): Linear matter power spectrum at the specified scale and redshift
+
+        """
+        return self.Pk_cb_interp(zs, ks)
+
     def growth_factor(self, zs, ks) -> np.ndarray:
         r"""
-        Calculate the growth factor for given redshifts and wavenumbers.
+        Calculate the total (dark matter + baryons + neutrinos) linear  growth factor for given redshifts and wavenumbers.
 
         $$
             D(z, k) =\sqrt{P_{\rm \delta\delta}(z, k)\
-            /P_{\rm \delta\delta}(z=0, k)}\\
+            /P_{\rm \delta\delta}(z=0, k)}
         $$
 
         and normalizes as for $D(z)/D(0)$.
@@ -262,31 +428,111 @@ class BACCOemuNonLinearPerturbations:
             ks (array_like): Wavenumbers at which to calculate the growth factor.
 
         Returns:
-            (np.ndarray): The growth factor as a function of redshift and wavenumber.
+            (np.ndarray): The linear  growth factor as a function of redshift and wavenumber.
         """
-        if hasattr(self, "Pk_interp") and self.Pk_interp is not None:
-            D_z_k = np.sqrt(self.Pk_interp(zs, ks) / self.Pk_interp(0, ks))
+        return self.linearperturbations.growth_factor(zs, ks)
+
+    def growth_rate(self, zs, ks) -> np.ndarray:
+        r"""
+        Calculate the total (dark matter + baryons + neutrinos) linear growth rate for given redshifts and wavenumbers.
+
+        Args:
+            zs (array_like): Redshifts at which to calculate the growth factor.
+            ks (array_like): Wavenumbers at which to calculate the growth factor.
+
+        Returns:
+            (np.ndarray): The linear growth rate as a function of redshift and wavenumber.
+        """
+        return self.linearperturbations.growth_rate(zs, ks)
+
+    def growth_factor_cb(self, zs, ks) -> np.ndarray:
+        r"""
+        Calculate the cold (dark matter + baryons) linear growth factor for given redshifts and wavenumbers.
+
+        $$
+            D(z, k) =\sqrt{P_{\rm \delta\delta}(z, k)\
+            /P_{\rm \delta\delta}(z=0, k)}
+        $$
+
+        and normalizes as for $D(z)/D(0)$.
+
+        Args:
+            zs (array_like): Redshifts at which to calculate the growth factor.
+            ks (array_like): Wavenumbers at which to calculate the growth factor.
+
+        Returns:
+            (np.ndarray): The linear growth factor as a function of redshift and wavenumber.
+        """
+        if hasattr(self, "Pk_cb_interp") and self.Pk_cb_interp is not None:
+            D_z_k = np.sqrt(self.Pk_cb_interp(zs, ks) / self.Pk_cb_interp(0, ks))
 
         return D_z_k
 
+    def growth_rate_cb(self, zs, ks) -> np.ndarray:
+        r"""
+        Calculate the cold (dark matter + baryons) linear growth rate for given redshifts and wavenumbers.
+
+        Args:
+            zs (array_like): Redshifts at which to calculate the growth factor.
+            ks (array_like): Wavenumbers at which to calculate the growth factor.
+
+        Returns:
+            (np.ndarray): The linear cold growth rate as a function of redshift and wavenumber.
+        """
+        growth = self.growth_factor_cb(zs, ks)
+        lna = np.log(1 / (1 + zs))
+
+        # sort by increasing lna
+        idx = np.argsort(lna)
+        lna_sorted = lna[idx]
+        growth_sorted = growth[idx, :]
+
+        cs = interpolate.CubicSpline(lna_sorted, np.log(growth_sorted), axis=0)
+        f = cs(lna_sorted, 1)  # d ln D / d ln a, shape (nz, nk)
+
+        unsort = np.argsort(idx)
+        return f[unsort, :]
+
     def sigma8_0(self) -> float:
         """
-        Calculate the sigma8 value for the current cosmology.
+        Calculate the total (dark matter + baryons + neutrinos) linear sigma8 value for the current cosmology.
 
         Returns:
         --------
         float
             The sigma8 value.
         """
-        return self.emu.get_sigma8(cold=self.cold, **self.params_emu)
+        return np.asarray(self.emu.get_sigma8(cold=False, **self.params_emu))[0]
 
     def sigma12_0(self) -> float:
         """
-        Calculate the sigma12 value for the current cosmology.
+        Calculate the total (dark matter + baryons + neutrinos) linear  sigma12 value for the current cosmology.
 
         Returns:
         --------
         float
             The sigma8 value.
         """
-        return self.emu.get_sigma12(cold=self.cold, **self.params_emu)
+        return np.asarray(self.emu.get_sigma12(cold=False, **self.params_emu))[0]
+
+    def sigma8_0_cb(self) -> float:
+        """
+        Calculate the  cold (dark matter + baryons) linear  sigma8 value for the current cosmology.
+
+        Returns:
+        --------
+        float
+            The sigma8 value.
+        """
+        return np.asarray(self.emu.get_sigma8(cold=True, **self.params_emu))[0]
+
+    def sigma12_0_cb(self) -> float:
+        """
+        Calculate the  cold (dark matter + baryons) linear sigma12 value for the current cosmology.
+
+        Returns:
+        --------
+        float
+            The sigma8 value.
+        """
+        return np.asarray(self.emu.get_sigma12(cold=True, **self.params_emu))[0]

d2ffe50395d108668984322ee898af89faf8ecc4
Ben Bose <45853389+nebblu@users.noreply.github.com>
2026-03-24 08:37:33 +0000

    Refactor Omega_nu and Omega_m calculations
    Set Omega_nu to 0 for the boost while keeping total matter the same as the real cosmology as decided in the modelling TH1 telecon on 23.03.2026

--- cloelib/cosmology/ReACTEmu_cosmology.py
+++ cloelib/cosmology/ReACTEmu_cosmology.py
@@ -192,8 +192,11 @@ class MGemuNonlinearBoost:
         zvals_inrange = zvals[(zvals >= zmin) & (zvals <= zmax)]
 
         # common cosmology block
-        Omega_nu = self.background.mnu / 93.14 / (self.background.H0 / 100.0) ** 2
-        Omega_m = self.background.Omega_cdm0 + self.background.Omega_b0 + Omega_nu
+        Omega_nu_real = self.background.mnu / 93.14 / (self.background.H0 / 100.0) ** 2
+        Omega_m = self.background.Omega_cdm0 + self.background.Omega_b0 + Omega_nu_real
+    
+        # Fixing massive neutrinos to 0 for the boost as decided in TH1-4 telecon, 23.03.2026 but keep same total Om
+        Omega_nu = 1e-10 #self.background.mnu / 93.14 / (self.background.H0 / 100.0) ** 2
 
         # defaults for MG/DE parameters (overridden below per model)
         w0 = self.background.w0

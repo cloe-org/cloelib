252e652c04965ec9205df21c6ded4d47afb402a6
zizgitlab <ziad.sakr@net.usj.edu.lb>
2026-04-16 23:47:19 +0200

    mgclass style fix

--- cloelib/cosmology/mgclass_cosmology.py
+++ cloelib/cosmology/mgclass_cosmology.py
@@ -277,14 +277,14 @@ class MGCLASSBackground:
                 [
                     (self.results.Omega_b() + self.results.Omega0_cdm())
                     * (1 + z) ** 3.0
-                    / (self.hubble_parameter(z)/self.results.H0) ** 0.5
+                    / (self.hubble_parameter(z) / self.results.H0) ** 0.5
                     for z in zs
                 ]
             )
         except TypeError:
-            (self.results.Omega_b() + self.results.Omega0_cdm()) * (
-                1 + zs
-            ) ** 3.0 / (self.hubble_parameter(zs)/self.results.H0) ** 0.5
+            (self.results.Omega_b() + self.results.Omega0_cdm()) * (1 + zs) ** 3.0 / (
+                self.hubble_parameter(zs) / self.results.H0
+            ) ** 0.5
         return Omegacb
 
     def Omega_m(self, zs: np.ndarray) -> np.ndarray:
@@ -318,7 +318,7 @@ class MGCLASSBackground:
                 [
                     self.results.Omega_b()
                     * (1 + z) ** 3.0
-                    / (self.hubble_parameter(z)/self.results.H0) ** 0.5
+                    / (self.hubble_parameter(z) / self.results.H0) ** 0.5
                     for z in zs
                 ]
             )
@@ -326,7 +326,7 @@ class MGCLASSBackground:
             Omegab = (
                 self.results.Omega_b()
                 * (1 + zs) ** 3.0
-                / (self.hubble_parameter(zs)/self.results.H0) ** 0.5
+                / (self.hubble_parameter(z) / self.results.H0) ** 0.5
             )
         return Omegab
 

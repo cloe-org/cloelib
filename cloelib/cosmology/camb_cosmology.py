8cffc3bd0168b099a6417aa9efb7e103d0b1d495
Ivan Sladoljev <ivan.sladoljev@gmail.com>
2026-04-21 16:31:42 +0100

    linearperturbations now typed as Optional[CAMBLinearPerturbations]

--- cloelib/cosmology/camb_cosmology.py
+++ cloelib/cosmology/camb_cosmology.py
@@ -451,7 +451,7 @@ class CAMBNonLinearPerturbations:
     def __init__(
         self,
         background: Background,
-        linearperturbations: Optional[object],
+        linearperturbations: Optional["CAMBLinearPerturbations"],
         redshifts: np.ndarray,
         nonlinear_model: Optional[str] = None,
         log10TAGN: Optional[float] = None,

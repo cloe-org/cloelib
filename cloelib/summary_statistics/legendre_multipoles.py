eb8283efa286971d708de3845d63af580e24acd7
Martin Kaercher <mkaerche@login07.leonardo.local>
2026-04-27 13:45:35 +0200

    Added 2PCF multipoles term method

--- cloelib/summary_statistics/legendre_multipoles.py
+++ cloelib/summary_statistics/legendre_multipoles.py
@@ -574,11 +574,11 @@ class LegendreMultipoles:
         s: np.ndarray,
         ells: Optional[np.ndarray] = None,
         use_AP: Optional[bool] = True,
-        logkmin: Optional[float] = -5,
-        logkmax: Optional[float] = 2,
+        logkmin: Optional[float] = -5.0,
+        logkmax: Optional[float] = 2.0,
         nk: Optional[int] = 2048,
         kcut: Optional[float] = 0.4,
-        pow: Optional[float] = 2,
+        pow: Optional[float] = 2.0,
         format_type: Optional[str] = None,
     ) -> dict:
         r"""Two-point correlation function Legendre multipoles.
@@ -614,7 +614,7 @@ class LegendreMultipoles:
             )
 
         ells = self._ensure_array(ells) if ells is not None else np.array([0, 2, 4])
-        k_hnkl = np.logspace(logkmin, logkmax, nk)
+        k_hnkl = np.logspace(logkmin, logkmax, nk) * self.spectro_power.background.h
         pk_multipoles = self.power_multipoles(k=k_hnkl, ells=ells, use_AP=use_AP)
         volume_factor = (k_hnkl**3) / (2 * (np.pi**2))
         xi_multipoles = {}
@@ -622,7 +622,7 @@ class LegendreMultipoles:
             y_array = (
                 volume_factor
                 * pk_multipoles[f"ell{ell}"]
-                * self._UVcutoff(k=k_hnkl, kcut=kcut, pow=pow)
+                * self._UVcutoff(k=k_hnkl, kcut=kcut*self.spectro_power.background.h, pow=pow)
                 * np.real(1j**ell)
             )
             transformer = fftlog(x=k_hnkl, fx=y_array, nu=2)
@@ -684,3 +684,69 @@ class LegendreMultipoles:
         )
 
         return xi_polar
+
+    def two_point_correlation_term_multipoles(
+        self,
+        s: np.ndarray,
+        term_list: list,
+        ells: Optional[np.ndarray] = None,
+        use_AP: Optional[bool] = True,
+        logkmin: Optional[float] = -5,
+        logkmax: Optional[float] = 2,
+        nk: Optional[int] = 2048,
+        kcut: Optional[float] = 0.4,
+        pow: Optional[float] = 2,
+    ) -> dict:
+
+        r"""Two-point correlation function Legendre multipoles of specified terms.
+
+        Parameters
+        ----------
+        s: np.ndarray
+            Comoving separations
+        term_list: list
+            List of terms to compute
+        ells: np.ndarray
+            Legendre multipole order
+        use_AP: bool
+            Flag to switch between with and without AP corrections
+        logkmin: float
+            Left logarithmic edge of input wave mode array
+        logkmax: float
+            Right logarithmic edge of input wave mode array
+        nk: int
+            Number of logarithmic wave mode bins
+        kcut: float
+            Cutoff scale for exponential damping
+        pow: float
+            Power index for exponential damping
+        Returns
+        -------
+        multipoles: dict
+            Two-point correlation function Legendre multipoles of specified terms
+        """
+
+        if self.spectro_power.NLcode != "COMET":
+            raise ValueError(
+                "2PCF multipoles for specific terms can temporarily be retrieved only with COMET"
+            )
+
+        ells = self._ensure_array(ells) if ells is not None else np.array([0, 2, 4])
+        k_hnkl = np.logspace(logkmin, logkmax, nk)
+        pk_multipoles = self.power_term_multipoles(k=k_hnkl, term_list=term_list, ells=ells, use_AP=use_AP)
+        volume_factor = (k_hnkl**3) / (2 * (np.pi**2))
+        xi_multipoles = {}
+        for ell in ells:
+            xi_temp = np.zeros((len(term_list), len(s)))
+            for term_id, term in enumerate(term_list):
+                y_array = (
+                    volume_factor
+                    * pk_multipoles[f"ell{ell}"][term_id]
+                    * self._UVcutoff(k=k_hnkl, kcut=kcut, pow=pow)
+                    * np.real(1j**ell)
+                )
+                transformer = fftlog(x=k_hnkl, fx=y_array, nu=2)
+                r_grid, transformed_log = transformer.fftlog(ell=ell)
+                xi_temp[term_id,:] = np.interp(s, r_grid, transformed_log)
+            xi_multipoles[f"ell{ell}"] = xi_temp
+        return xi_multipoles

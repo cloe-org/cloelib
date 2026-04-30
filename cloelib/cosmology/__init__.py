8410632bb32b2acb437e414c68a2296cf416dc2d
sankarshana16 <sankarshana16@gmail.com>
2026-04-30 18:39:41 +0530

    Fixed init file

---
+++ cloelib/cosmology/__init__.py
@@ -0,0 +1,11 @@
+"""Cosmology package of cloelib.
+
+The package provides interface with external Boltzmann solvers
+or emulators via Python Protocols following the cosmology.API.
+Core structure enables defining Background & Perturbation models.
+
+Supported External Codes:
+
+- **Background**: `camb`, `class`
+- **Perturbations**: `camb`, `class`, `HMCode2020emu`, `euclidemu2`, `baccoemu`
+"""

1167a06446e0d95ae22ec2d15015c33f882364a1
Casper Vedder <caspervedder@gmail.com>
2026-05-01 17:32:37 +0200

    small dict to array fix

--- cloelib/summary_statistics/angular_two_point.py
+++ cloelib/summary_statistics/angular_two_point.py
@@ -145,8 +145,9 @@ def get_cosebis_from_cl(cells, ells, w_ell, ns, software=None):
     """
     if software is None:
         software = "get_cosebis_from_cl (cloelib)"
-    w_ell = np.asarray(w_ell)
-    ns = np.asarray(ns)
+    print("ok")
+    w_ell = np.array(list(w_ell.values()))
+    ns = np.asarray(ns - 1)
     weights = simpsons_weights_jit(len(ells))
     tomo_cosebis = {}
 

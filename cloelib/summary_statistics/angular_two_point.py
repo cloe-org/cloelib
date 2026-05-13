3f1c4aaab20d28aa0363816479aa6bf9423377e3
NastassiaG <107264848+NastassiaG@users.noreply.github.com>
2026-05-13 09:54:39 +0100

    Update angular_two_point.py
    Check tracer_types with isinstance for compatibility with child classes of PositionTracer

--- cloelib/summary_statistics/angular_two_point.py
+++ cloelib/summary_statistics/angular_two_point.py
@@ -397,8 +397,17 @@ class AngularTwoPoint:
             (ShearTracer, PositionsTracer): ("POS", "SHE"),
             (ShearTracer, ShearTracer): ("SHE", "SHE"),
         }
+
+        # ADDED FOR WEYL PROJECT: accept subclasses by scanning with isinstance
+        if tracer_types not in tracer_keys:
+            for (A, B), fn in tracer_keys.items():
+                if isinstance(self.tracer1, A) and isinstance(self.tracer2, B):
+                    tracer_types = (A, B)
+                    break
+        
         if tracer_types not in tracer_keys:
             raise ValueError("Unsupported tracer pair for mixing matrix.")
+        
         key_type = tracer_keys[tracer_types]
         ellmax = mixing_matrix[key_type + (1, 1)].shape[
             -1

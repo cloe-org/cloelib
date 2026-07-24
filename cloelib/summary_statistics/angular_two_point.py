6d3605e1e4b43f6a841acdb628b8948c01e4bb8e
Yu-Ting Chang <67809413+ytchang05@users.noreply.github.com>
2026-07-24 15:36:50 +0200

    angular two point updates for gw observables

--- cloelib/summary_statistics/angular_two_point.py
+++ cloelib/summary_statistics/angular_two_point.py
@@ -5,6 +5,7 @@ from cloelib.observables.tracer import Tracer
 from cloelib.observables.photo import PositionsTracer
 from cloelib.observables.photo import ShearTracer
 from cloelib.observables.cmb import CMBLensingTracer
+from cloelib.observables.gw import GWNumberCountsTracer, GWWeakLensingTracer
 from cloelib.auxiliary.units import SPEED_OF_LIGHT
 from cloelib.auxiliary.math_utils import simpsons_weights_jit
 from cloelib.profiling import profile_function
@@ -604,6 +605,29 @@ class AngularTwoPoint:
             a, b = sorted((i, j))
             return {("CMBL", "SHE", a, b): np.stack([block, np.zeros_like(block)])}
 
+        def gwnc_gwnc_rule(C, i, j):
+            return {("GWNC", "GWNC", i, j): C[:, i - 1, j - 1]}
+
+        def gwwl_gwwl_rule(C, i, j):
+            return {("GWWL", "GWWL", i, j): C[:, i - 1, j - 1]}
+
+        def gwnc_gwwl_rule(C, i, j):
+            return {("GWNC", "GWWL", i, j): C[:, i - 1, j - 1]}
+
+        def pos_gwnc_rule(C, i, j):
+            return {("POS", "GWNC", i, j): C[:, i - 1, j - 1]}
+
+        def pos_gwwl_rule(C, i, j):
+            return {("POS", "GWWL", i, j): C[:, i - 1, j - 1]}
+
+        def she_gwnc_rule(C, i, j):
+            block = C[:, i - 1, j - 1]
+            return {("SHE", "GWNC", i, j): np.stack([block, np.zeros_like(block)])}
+
+        def she_gwwl_rule(C, i, j):
+            block = C[:, i - 1, j - 1]
+            return {("SHE", "GWWL", i, j): np.stack([block, np.zeros_like(block)])}
+
         tracer_rules = {
             (PositionsTracer, PositionsTracer): pos_pos_rule,
             (PositionsTracer, ShearTracer): pos_she_rule,
@@ -611,6 +635,13 @@ class AngularTwoPoint:
             (CMBLensingTracer, PositionsTracer): cmbl_pos_rule,
             (CMBLensingTracer, ShearTracer): cmbl_she_rule,
             (CMBLensingTracer, CMBLensingTracer): cmbl_cmbl_rule,
+            (GWNumberCountsTracer, GWNumberCountsTracer): gwnc_gwnc_rule,
+            (GWWeakLensingTracer, GWWeakLensingTracer): gwwl_gwwl_rule,
+            (GWNumberCountsTracer, GWWeakLensingTracer): gwnc_gwwl_rule,
+            (PositionsTracer, GWNumberCountsTracer): pos_gwnc_rule,
+            (PositionsTracer, GWWeakLensingTracer): pos_gwwl_rule,
+            (ShearTracer, GWNumberCountsTracer): she_gwnc_rule,
+            (ShearTracer, GWWeakLensingTracer): she_gwwl_rule,
         }
 
         # normalize the key so (A, B) and (B, A) are both supported
@@ -624,18 +655,28 @@ class AngularTwoPoint:
                 f"No rule defined for tracers {type(self.tracer1)}, {type(self.tracer2)}"
             )
 
-        # Vectorized update of C_ell_out using dictionary comprehensions
-        a, b = sorted((n_bin1, n_bin2))
-        C_ell_out = {
-            k: v
-            for i in range(1, a + 1)
-            for j in range(i, b + 1)
-            for k, v in (
-                rule_fn(C_ell_calc, i, j)
-                if n_bin1 <= n_bin2
-                else rule_fn(C_ell_calc, j, i)
-            ).items()
-        }
+        gw_types = (GWNumberCountsTracer, GWWeakLensingTracer)
+        contains_gw = key[0] in gw_types or key[1] in gw_types
+        if contains_gw:
+            same_tracer = key[0] is key[1]
+            C_ell_out = {
+                k: v
+                for i in range(1, n_bin1 + 1)
+                for j in range(i if same_tracer else 1, n_bin2 + 1)
+                for k, v in rule_fn(C_ell_calc, i, j).items()
+            }
+        else:
+            a, b = sorted((n_bin1, n_bin2))
+            C_ell_out = {
+                k: v
+                for i in range(1, a + 1)
+                for j in range(i, b + 1)
+                for k, v in (
+                    rule_fn(C_ell_calc, i, j)
+                    if n_bin1 <= n_bin2
+                    else rule_fn(C_ell_calc, j, i)
+                ).items()
+            }
 
         # Use dictionary comprehension for cosmolib_Cls creation
         return {
